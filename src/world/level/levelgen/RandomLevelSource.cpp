5edb0bac0d5670483468885b66081c8251fc67bc
MFSCelebrate <19358185571@163.com>
2026-03-22 13:34:50 +0800

    Update RandomLevelSource.cpp

--- src/world/level/levelgen/RandomLevelSource.cpp
+++ src/world/level/levelgen/RandomLevelSource.cpp
@@ -29,7 +29,7 @@ RandomLevelSource::RandomLevelSource(Level* level, long seed, int version, bool 
 	forestNoise(&random, 8),
 	spawnMobs(spawnMobs),
 	pnr(NULL), ar(NULL), br(NULL), sr(NULL), dr(NULL), fi(NULL), fis(NULL),
-	offsetX(12550800), offsetZ(12550800)   // 新增
+	offsetX(3137705), offsetZ(3137705)   // 新增
 {
 	for (int i=0; i<32; ++i)
 	for (int j=0; j<32; ++j)
