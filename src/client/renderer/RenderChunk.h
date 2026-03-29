041e82e8b5fcf25202ff017a8fa0148006c6644a
MFSCelebrate <19358185571@163.com>
2026-03-29 13:25:21 +0800

    Update RenderChunk.h

--- src/client/renderer/RenderChunk.h
+++ src/client/renderer/RenderChunk.h
@@ -6,16 +6,18 @@
 #include "gles.h"
 #include "../../world/phys/Vec3.h"
 
+// RenderChunk.h
 class RenderChunk
 {
 public:
-	RenderChunk();
-	RenderChunk(GLuint vboId_, int vertexCount_);
+    RenderChunk();
+    RenderChunk(GLuint vboId_, int vertexCount_);
 
-	GLuint vboId;
-	GLsizei vertexCount;
-	int id;
-	Vec3 pos;
+    GLuint vboId;
+    GLsizei vertexCount;
+    int id;
+    Vec3 pos;          // 保留，可能用于其他用途
+    int baseX, baseY, baseZ;   // 新增：区块的整型世界坐标（方块单位）
 
 private:
 	static int runningId;
