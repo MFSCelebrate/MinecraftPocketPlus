c96c51095809e1ac2ebea334bf3c58de5cde55cd
MFSCelebrate <19358185571@163.com>
2026-03-29 12:33:10 +0800

    Update RenderList.h

--- src/client/renderer/RenderList.h
+++ src/client/renderer/RenderList.h
@@ -10,32 +10,33 @@ class RenderList
 	static const int MAX_NUM_OBJECTS = 1024 * 3;
 
 public:
-	RenderList();
-	~RenderList();
+    RenderList();
+    ~RenderList();
 
     void init(float xOff, float yOff, float zOff);
 
-	void add(int list);
-	void addR(const RenderChunk& chunk);
-
-	__inline void next() { ++listIndex; }
+    void add(int list);
+    void addR(const RenderChunk& chunk);
+    void next() { ++listIndex; }
 
     void render();
-	void renderChunks();
-
+    void renderChunks();
     void clear();
 
+    // 新增：设置是否使用相对平移
+    void setUseRelativeTranslation(bool use) { m_useRelativeTranslation = use; }
 
-	float xOff, yOff, zOff;
-	int* lists;
-	RenderChunk* rlists;
+    float xOff, yOff, zOff;
+    int* lists;
+    RenderChunk* rlists;
 
-	int listIndex;
-	bool inited;
-	bool rendered;
+    int listIndex;
+    bool inited;
+    bool rendered;
 
 private:
-	int bufferLimit;
+    int bufferLimit;
+    bool m_useRelativeTranslation;   // 新增
 };
 
 #endif /*NET_MINECRAFT_CLIENT_RENDERER__RenderList_H__*/
