49cc90c0e3c7779b9b0e6cc982df02ab5f01d9aa
MFSCelebrate_ <19358185571@163.com>
2026-04-04 19:22:13 +0800

    Update ExternalFileLevelStorage.h

--- src/world/level/storage/ExternalFileLevelStorage.h
+++ src/world/level/storage/ExternalFileLevelStorage.h
@@ -3,12 +3,8 @@
 #ifndef NET_MINECRAFT_WORLD_LEVEL_STORAGE__ExternalFileLevelStorage_H__
 #define NET_MINECRAFT_WORLD_LEVEL_STORAGE__ExternalFileLevelStorage_H__
 
-//package net.minecraft.world.level.storage;
-
 #include <vector>
 #include <list>
-
-//#include "com/mojang/nbt/CompoundTag.h"
 #include "LevelStorage.h"
 #include "../chunk/storage/ChunkStorage.h"
 
@@ -16,71 +12,54 @@ class Player;
 class Dimension;
 class RegionFile;
 
-
-typedef struct UnsavedLevelChunk
-{
-	int				pos;
-	RakNet::TimeMS	addedToList;
-	LevelChunk*		chunk;
+typedef struct UnsavedLevelChunk {
+    int pos;
+    RakNet::TimeMS addedToList;
+    LevelChunk* chunk;
 } UnsavedLevelChunk;
 
 typedef std::list<UnsavedLevelChunk> UnsavedChunkList;
 
-/*public*/
-class ExternalFileLevelStorage:
-		public LevelStorage,
-		public ChunkStorage
-		//public PlayerIO
-{
+class ExternalFileLevelStorage : public LevelStorage, public ChunkStorage {
 public:
     ExternalFileLevelStorage(const std::string& levelId, const std::string& fullPath);
-	virtual ~ExternalFileLevelStorage();
+    virtual ~ExternalFileLevelStorage();
 
     LevelData* prepareLevel(Level* level);
-
-	//throws LevelConflictException
     void checkSession() {}
-
     ChunkStorage* createChunkStorage(Dimension* dimension) { return this; }
-
     void saveLevelData(LevelData& levelData, std::vector<Player*>* players);
-    // PlayerIO getPlayerIO() { return this; }
-	// CompoundTag loadPlayerDataTag(std::string playerName) { return NULL; }
-
     void closeAll() {}
 
-	static bool readLevelData(const std::string& directory, LevelData& dest);
-	static bool readPlayerData(const std::string& filename, LevelData& dest);
-	static bool writeLevelData(const std::string& datFileName, LevelData& dest, const std::vector<Player*>* players);
-	static void saveLevelData(const std::string& directory, LevelData& levelData, std::vector<Player*>* players);
+    static bool readLevelData(const std::string& directory, LevelData& dest);
+    static bool readPlayerData(const std::string& filename, LevelData& dest);
+    static bool writeLevelData(const std::string& datFileName, LevelData& dest, const std::vector<Player*>* players);
+    static void saveLevelData(const std::string& directory, LevelData& levelData, std::vector<Player*>* players);
 
     int savePendingUnsavedChunks(int maxCount);
 
-	//
-	// ChunkStorage methods
-	//
-	virtual LevelChunk* load(Level* level, int x, int z);
-	void save(Level* level, LevelChunk* levelChunk);
-	// @note, loadEntities and saveEntities dont use second parameter
-	void loadEntities(Level* level, LevelChunk* levelChunk);
-	void saveEntities(Level* level, LevelChunk* levelChunk);
-	void saveGame(Level* level);
-    void saveAll(Level* level, std::vector<LevelChunk*>& levelChunks);
+    // ChunkStorage methods (坐标改为 int64_t)
+    virtual LevelChunk* load(Level* level, int64_t x, int64_t z) override;
+    void save(Level* level, LevelChunk* levelChunk) override;
+    void loadEntities(Level* level, LevelChunk* levelChunk) override;
+    void saveEntities(Level* level, LevelChunk* levelChunk) override;
+    void saveGame(Level* level) override;
+    void saveAll(Level* level, std::vector<LevelChunk*>& levelChunks) override;
 
-	virtual void tick();
-	virtual void flush() {}
+    virtual void tick() override;
+    virtual void flush() override {}
+
 private:
-	std::string levelId;
-	std::string levelPath;
-	LevelData* loadedLevelData;
-	RegionFile* regionFile;
-	RegionFile* entitiesFile;
-
-	Level* level;
-	int tickCount;
-	int loadedStorageVersion;
-	UnsavedChunkList unsavedChunkList;
-	int lastSavedEntitiesTick;
+    std::string levelId;
+    std::string levelPath;
+    LevelData* loadedLevelData;
+    RegionFile* regionFile;
+    RegionFile* entitiesFile;
+    Level* level;
+    int tickCount;
+    int loadedStorageVersion;
+    UnsavedChunkList unsavedChunkList;
+    int lastSavedEntitiesTick;
 };
 
 #endif /*NET_MINECRAFT_WORLD_LEVEL_STORAGE__ExternalFileLevelStorage_H__*/
