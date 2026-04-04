91ab863d6f3fad7ff4bed4267425231a3e0e22f6
MFSCelebrate_ <19358185571@163.com>
2026-04-05 00:43:39 +0800

    Update Region.h

--- src/world/level/Region.h
+++ src/world/level/Region.h
@@ -1,9 +1,8 @@
 #ifndef NET_MINECRAFT_WORLD_LEVEL__Region_H__
 #define NET_MINECRAFT_WORLD_LEVEL__Region_H__
 
-//package net.minecraft.world.level;
-
 #include "LevelSource.h"
+#include <cstdint>
 
 class Level;
 class Material;
@@ -12,28 +11,29 @@ class LevelChunk;
 class Region: public LevelSource
 {
 public:
-    Region(Level* level, int x1, int y1, int z1, int x2, int y2, int z2);
-	~Region();
+    Region(Level* level, int64_t x1, int y1, int64_t z1, int64_t x2, int y2, int64_t z2);
+    ~Region();
 
-	bool isSolidRenderTile(int x, int y, int z);
-    bool isSolidBlockingTile(int x, int y, int z);
-	int  getTile(int x, int y, int z);
-	bool isEmptyTile(int x, int y, int z);
+    // 实现 LevelSource 的纯虚函数（参数改为 int64_t）
+    virtual int getTile(int64_t x, int y, int64_t z) override;
+    virtual bool isEmptyTile(int64_t x, int y, int64_t z) override;
+    virtual float getBrightness(int64_t x, int y, int64_t z) override;
+    virtual int getData(int64_t x, int y, int64_t z) override;
+    virtual const Material* getMaterial(int64_t x, int y, int64_t z) override;
+    virtual bool isSolidRenderTile(int64_t x, int y, int64_t z) override;
+    virtual bool isSolidBlockingTile(int64_t x, int y, int64_t z) override;
+    virtual Biome* getBiome(int64_t x, int64_t z) override;
 
-	float getBrightness(int x, int y, int z);
-    int   getRawBrightness(int x, int y, int z);
-	int   getRawBrightness(int x, int y, int z, bool propagate);
+    // 辅助函数（内部使用）
+    int getRawBrightness(int64_t x, int y, int64_t z);
+    int getRawBrightness(int64_t x, int y, int64_t z, bool propagate);
 
-	int getData(int x, int y, int z);
-	const Material* getMaterial(int x, int y, int z);
-	Biome* getBiome(int x, int z);
 private:
-    int xc1, zc1;
+    int64_t xc1, zc1;           // 区块坐标（单位：区块）
     LevelChunk*** chunks;
     Level* level;
-
-	int size_x;
-	int size_z;
+    int size_x;                 // 区块范围大小（int 足够，因为区域不会太大）
+    int size_z;
 };
 
-#endif /*NET_MINECRAFT_WORLD_LEVEL__Region_H__*/
+#endif
