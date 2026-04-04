c6f42576b0f2dd5b73c54162207140e27b11f53d
MFSCelebrate <19358185571@163.com>
2026-04-04 12:15:28 +0800

    Update MovePlayerPacket.h

--- src/network/packet/MovePlayerPacket.h
+++ src/network/packet/MovePlayerPacket.h
@@ -6,49 +6,37 @@
 class MovePlayerPacket : public Packet
 {
 public:
+    int entityId;
+    double x, y, z;
+    double xd, yd, zd;   // 速度（新增）
+    float xRot, yRot;
 
-	int entityId;
-	float x, y, z, xRot, yRot;
+    MovePlayerPacket() {}
+    MovePlayerPacket(int entityId, double x, double y, double z,
+                     double xd, double yd, double zd,
+                     float yRot, float xRot)
+        : entityId(entityId), x(x), y(y), z(z),
+          xd(xd), yd(yd), zd(zd),
+          yRot(yRot), xRot(xRot) {}
 
-	MovePlayerPacket()
-	{
-	}
+    void write(RakNet::BitStream* bitStream) {
+        bitStream->Write((RakNet::MessageID)(ID_USER_PACKET_ENUM + PACKET_MOVEPLAYER));
+        bitStream->Write(entityId);
+        bitStream->Write(x); bitStream->Write(y); bitStream->Write(z);
+        bitStream->Write(xd); bitStream->Write(yd); bitStream->Write(zd);
+        bitStream->Write(yRot); bitStream->Write(xRot);
+    }
 
-	MovePlayerPacket(int entityId, float x, float y, float z, float xRot, float yRot)
-	:	entityId(entityId),
-		x(x),
-		y(y),
-		z(z),
-		xRot(xRot),
-		yRot(yRot)
-	{}
+    void read(RakNet::BitStream* bitStream) {
+        bitStream->Read(entityId);
+        bitStream->Read(x); bitStream->Read(y); bitStream->Read(z);
+        bitStream->Read(xd); bitStream->Read(yd); bitStream->Read(zd);
+        bitStream->Read(yRot); bitStream->Read(xRot);
+    }
 
-	void write(RakNet::BitStream* bitStream)
-	{
-		bitStream->Write((RakNet::MessageID)(ID_USER_PACKET_ENUM + PACKET_MOVEPLAYER));
-
-		bitStream->Write(entityId);
-		bitStream->Write(x);
-		bitStream->Write(y);
-		bitStream->Write(z);
-		bitStream->Write(yRot);
-		bitStream->Write(xRot);
-	}
-
-	void read(RakNet::BitStream* bitStream)
-	{
-		bitStream->Read(entityId);
-		bitStream->Read(x);
-		bitStream->Read(y);
-		bitStream->Read(z);
-		bitStream->Read(yRot);
-		bitStream->Read(xRot);
-	}
-
-	void handle(const RakNet::RakNetGUID& source, NetEventCallback* callback)
-	{
-		callback->handle(source, (MovePlayerPacket*)this);
-	}
+    void handle(const RakNet::RakNetGUID& source, NetEventCallback* callback) {
+        callback->handle(source, this);
+    }
 };
 
-#endif /*NET_MINECRAFT_NETWORK_PACKET__MovePlayerPacket_H__*/
+#endif
