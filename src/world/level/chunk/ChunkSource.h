3ee1970fea9cfa1cfe2f141a981bd355c483acec
MFSCelebrate_ <19358185571@163.com>
2026-04-04 18:05:21 +0800

    Update ChunkSource.h

--- src/world/level/chunk/ChunkSource.h
+++ src/world/level/chunk/ChunkSource.h
@@ -1,8 +1,6 @@
 #ifndef NET_MINECRAFT_WORLD_LEVEL_CHUNK__ChunkSource_H__
 #define NET_MINECRAFT_WORLD_LEVEL_CHUNK__ChunkSource_H__
 
-//package net.minecraft.world.level.chunk;
-
 #include <string>
 #include "../biome/Biome.h"
 
@@ -12,35 +10,20 @@ class LevelChunk;
 class ChunkSource
 {
 public:
-	bool isChunkCache;
+    bool isChunkCache;
 
-	ChunkSource()
-	:	isChunkCache(false)
-	{}
+    ChunkSource() : isChunkCache(false) {}
+    virtual ~ChunkSource() {}
 
-	virtual ~ChunkSource(){}
-
-	virtual bool hasChunk(int x, int y) = 0;
-
-    virtual LevelChunk* getChunk(int x, int z) = 0;
-
-    virtual LevelChunk* create(int x, int z) = 0;
-
-    virtual void postProcess(ChunkSource* parent, int x, int z) = 0;
-
-    //virtual bool save(bool force, ProgressListener* progressListener) = 0;
-
+    virtual bool hasChunk(int64_t x, int64_t y) = 0;
+    virtual LevelChunk* getChunk(int64_t x, int64_t z) = 0;
+    virtual LevelChunk* create(int64_t x, int64_t z) = 0;
+    virtual void postProcess(ChunkSource* parent, int64_t x, int64_t z) = 0;
     virtual bool tick() = 0;
-
     virtual bool shouldSave() = 0;
-	virtual void saveAll(bool onlyUnsaved) {}
-
-	virtual Biome::MobList getMobsAt(const MobCategory& mobCategory, int x, int y, int z) = 0;
-
-    /**
-     * Returns some stats that are rendered when the user holds F3.
-     */
+    virtual void saveAll(bool onlyUnsaved) {}
+    virtual Biome::MobList getMobsAt(const MobCategory& mobCategory, int x, int y, int z) = 0;
     virtual std::string gatherStats() = 0;
 };
 
-#endif /*NET_MINECRAFT_WORLD_LEVEL_CHUNK__ChunkSource_H__*/
+#endif
