f33228b6be5fde90d2ab5051e581a49c1bebbd36
MFSCelebrate_ <19358185571@163.com>
2026-04-04 18:15:11 +0800

    Update RandomLevelSource.h

--- src/world/level/levelgen/RandomLevelSource.h
+++ src/world/level/levelgen/RandomLevelSource.h
@@ -19,15 +19,14 @@ class RandomLevelSource: public ChunkSource
     static const float SNOW_SCALE;
 
 public:
-// 在 public 区域添加（不标记 const）
-double getLPerlinNoise1(float x, float z) { return lperlinNoise1.getValue(x, z); }
-double getLPerlinNoise2(float x, float z) { return lperlinNoise2.getValue(x, z); }
-double getPerlinNoise1(float x, float z) { return perlinNoise1.getValue(x, z); }
-double getPerlinNoise2(float x, float z) { return perlinNoise2.getValue(x, z); }
-double getPerlinNoise3(float x, float z) { return perlinNoise3.getValue(x, z); }
-double getScaleNoise(float x, float z) { return scaleNoise.getValue(x, z); }
-double getDepthNoise(float x, float z) { return depthNoise.getValue(x, z); }
-double getForestNoise(float x, float z) { return forestNoise.getValue(x, z); }
+    double getLPerlinNoise1(float x, float z) { return lperlinNoise1.getValue(x, z); }
+    double getLPerlinNoise2(float x, float z) { return lperlinNoise2.getValue(x, z); }
+    double getPerlinNoise1(float x, float z) { return perlinNoise1.getValue(x, z); }
+    double getPerlinNoise2(float x, float z) { return perlinNoise2.getValue(x, z); }
+    double getPerlinNoise3(float x, float z) { return perlinNoise3.getValue(x, z); }
+    double getScaleNoise(float x, float z) { return scaleNoise.getValue(x, z); }
+    double getDepthNoise(float x, float z) { return depthNoise.getValue(x, z); }
+    double getForestNoise(float x, float z) { return forestNoise.getValue(x, z); }
 
     static const int CHUNK_HEIGHT = 8;
     static const int CHUNK_WIDTH = 4;
@@ -35,36 +34,32 @@ double getForestNoise(float x, float z) { return forestNoise.getValue(x, z); }
     RandomLevelSource(Level* level, long seed, int version, bool spawnMobs);
     ~RandomLevelSource();
 
-    bool hasChunk(int x, int y);
-    LevelChunk* create(int x, int z);
-    LevelChunk* getChunk(int xOffs, int zOffs);
+    bool hasChunk(int64_t x, int64_t y) override;
+    LevelChunk* create(int64_t x, int64_t z) override;
+    LevelChunk* getChunk(int64_t xOffs, int64_t zOffs) override;
+    void prepareHeights(int64_t xOffs, int64_t zOffs, unsigned char* blocks, void* biomes, float* temperatures);
+    void buildSurfaces(int64_t xOffs, int64_t zOffs, unsigned char* blocks, Biome** biomes);
+    void postProcess(ChunkSource* parent, int64_t xt, int64_t zt) override;
 
-    void prepareHeights(int xOffs, int zOffs, unsigned char* blocks, /*Biome*/void* biomes, float* temperatures);
-    void buildSurfaces(int xOffs, int zOffs, unsigned char* blocks, Biome** biomes);
-    void postProcess(ChunkSource* parent, int xt, int zt);
+    bool tick() override;
+    Biome::MobList getMobsAt(const MobCategory& mobCategory, int x, int y, int z) override;
+    bool shouldSave() override;
+    std::string gatherStats() override;
 
-    bool tick();
-
-    Biome::MobList getMobsAt(const MobCategory& mobCategory, int x, int y, int z);
-
-    bool shouldSave();
-    std::string gatherStats();
-
-    // 新增：获取地形偏移量
-    int getOffsetX() const { return offsetX; }
-    int getOffsetZ() const { return offsetZ; }
+    int64_t getOffsetX() const { return offsetX; }
+    int64_t getOffsetZ() const { return offsetZ; }
 
 private:
-    float* getHeights(float* buffer, int x, int y, int z, int xSize, int ySize, int zSize);
-    void calcWaterDepths(ChunkSource* parent, int xt, int zt);
+    float* getHeights(float* buffer, int64_t x, int y, int64_t z, int xSize, int ySize, int zSize);
+    void calcWaterDepths(ChunkSource* parent, int64_t xt, int64_t zt);
 
 public:
     LargeCaveFeature caveFeature;
     int waterDepths[16+16][16+16];
     int getSeaLevel() const { return customSeaLevel; }
+
 private:
     ChunkMap chunkMap;
-
     Random random;
     PerlinNoise lperlinNoise1;
     PerlinNoise lperlinNoise2;
@@ -90,28 +85,22 @@ private:
     float* fi;
     float* fis;
 
-    int customSeaLevel;     // 用户自定义海平面高度（0-127），默认63
-
-    // 地形偏移量（区块为单位）
-    int offsetX, offsetZ;   // 注意：此处为唯一声明，原有 offsetX, OffsetZ 已被替换
+    int customSeaLevel;
+    int64_t offsetX, offsetZ;
 };
 
 class PerformanceTestChunkSource : public ChunkSource
 {
     Level* level;
 public:
-    PerformanceTestChunkSource(Level* level)
-    :   ChunkSource(),
-        level(level)
-    {}
-
-    virtual bool hasChunk(int x, int y) { return true; };
-    virtual LevelChunk* getChunk(int x, int z) { return create(x, z); };
-    virtual LevelChunk* create(int x, int z);
-    virtual void postProcess(ChunkSource* parent, int x, int z) {};
-    virtual bool tick() { return false; };
-    virtual bool shouldSave() { return false; };
-    virtual std::string gatherStats() { return "PerformanceTestChunkSource"; };
+    PerformanceTestChunkSource(Level* level) : ChunkSource(), level(level) {}
+    virtual bool hasChunk(int64_t x, int64_t y) override { return true; }
+    virtual LevelChunk* getChunk(int64_t x, int64_t z) override { return create(x, z); }
+    virtual LevelChunk* create(int64_t x, int64_t z) override;
+    virtual void postProcess(ChunkSource* parent, int64_t x, int64_t z) override {}
+    virtual bool tick() override { return false; }
+    virtual bool shouldSave() override { return false; }
+    virtual std::string gatherStats() override { return "PerformanceTestChunkSource"; }
 };
 
 #endif
