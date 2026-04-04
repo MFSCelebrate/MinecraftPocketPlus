0a89e86062edc7c9f5d91ff8b56149333ba55289
MFSCelebrate_ <19358185571@163.com>
2026-04-05 00:15:32 +0800

    Update Entity.h

--- src/world/entity/Entity.h
+++ src/world/entity/Entity.h
@@ -1,7 +1,7 @@
 #ifndef NET_MINECRAFT_WORLD_ENTITY__Entity_H__
 #define NET_MINECRAFT_WORLD_ENTITY__Entity_H__
 
-//package net.minecraft.world.entity;
+#include <cstdint>
 
 class Level;
 class Player;
@@ -14,7 +14,6 @@ class CompoundTag;
 #include "EntityRendererId.h"
 #include "../phys/AABB.h"
 #include "../../SharedConstants.h"
-//#include "../../util/MemUtils.h"
 #include "../../util/Mth.h"
 #include "../../util/Random.h"
 
@@ -23,47 +22,47 @@ class SynchedEntityData;
 class Entity
 {
 public:
-	static int entityCounter;
-	static const int TOTAL_AIR_SUPPLY = 15 * SharedConstants::TicksPerSecond;
+    static int entityCounter;
+    static const int TOTAL_AIR_SUPPLY = 15 * SharedConstants::TicksPerSecond;
 
     Entity(Level* level);
-	virtual ~Entity();
+    virtual ~Entity();
 
-	void _init();
-	virtual void reset(); // { super::reset(); _init(); }
+    void _init();
+    virtual void reset();
 
-	int hashCode();
-	bool operator==(Entity& rhs);
+    int hashCode();
+    bool operator==(Entity& rhs);
 
-	virtual void setLevel(Level* level);
+    virtual void setLevel(Level* level);
     
-	virtual void remove();
+    virtual void remove();
 
     virtual void setPos(double x, double y, double z);
-virtual void move(double xa, double ya, double za);
-virtual void moveTo(double x, double y, double z, float yRot, float xRot);
-virtual void lerpTo(double x, double y, double z, float yRot, float xRot, int steps);
-virtual void lerpMotion(double xd, double yd, double zd);
-virtual void moveRelative(float xa, float za, float speed);
+    virtual void move(double xa, double ya, double za);
+    virtual void moveTo(double x, double y, double z, float yRot, float xRot);
+    virtual void lerpTo(double x, double y, double z, float yRot, float xRot, int steps);
+    virtual void lerpMotion(double xd, double yd, double zd);
+    virtual void moveRelative(float xa, float za, float speed);
 
     virtual void turn(float xo, float yo);
     virtual void interpolateTurn(float xo, float yo);
 
     virtual void tick();
-	virtual void baseTick();
+    virtual void baseTick();
 
-	virtual bool intersects(float x0, float y0, float z0, float x1, float y1, float z1);
+    virtual bool intersects(float x0, float y0, float z0, float x1, float y1, float z1);
     virtual bool isFree(float xa, float ya, float za, float grow);
     virtual bool isFree(float xa, float ya, float za);
-	virtual bool isInWall();
+    virtual bool isInWall();
     virtual bool isInWater();
-	virtual bool isInLava();
+    virtual bool isInLava();
     virtual bool isUnderLiquid(const Material* material);
 
-	virtual void makeStuckInWeb();
+    virtual void makeStuckInWeb();
 
-	virtual float getHeadHeight();
-	virtual float getShadowHeightOffs();
+    virtual float getHeadHeight();
+    virtual float getShadowHeightOffs();
 
     virtual float getBrightness(float a);
 
@@ -72,8 +71,8 @@ virtual void moveRelative(float xa, float za, float speed);
     float distanceToSqr(float x2, float y2, float z2);
     float distanceToSqr(Entity* e);
 
-	virtual bool interactPreventDefault();
-	virtual bool interact(Player* player);
+    virtual bool interactPreventDefault();
+    virtual bool interact(Player* player);
     virtual void playerTouch(Player* player);
 
     virtual void push(Entity* e);
@@ -83,144 +82,134 @@ virtual void moveRelative(float xa, float za, float speed);
     virtual bool isPushable();
     virtual bool isShootable();
 
-	virtual bool isSneaking();
+    virtual bool isSneaking();
 
-	virtual bool isAlive();
-	virtual bool isOnFire();
+    virtual bool isAlive();
+    virtual bool isOnFire();
 
-	virtual bool isPlayer();
-	virtual bool isCreativeModeAllowed();
+    virtual bool isPlayer();
+    virtual bool isCreativeModeAllowed();
 
     virtual bool shouldRender(Vec3& c);
     virtual bool shouldRenderAtSqrDistance(float distance);
 
-	virtual bool hurt(Entity* source, int damage);
+    virtual bool hurt(Entity* source, int damage);
     virtual void animateHurt();
 
     virtual void handleEntityEvent(char eventId) {}
 
- 	virtual float getPickRadius();
+    virtual float getPickRadius();
 
-	virtual ItemEntity* spawnAtLocation(int resource, int count);
-	virtual ItemEntity* spawnAtLocation(int resource, int count, float yOffs);
-	// @attn: right now this means a pointer that spawnAtLocation takes ownership of
-	virtual ItemEntity* spawnAtLocation(ItemInstance* itemInstance, float yOffs);
+    virtual ItemEntity* spawnAtLocation(int resource, int count);
+    virtual ItemEntity* spawnAtLocation(int resource, int count, float yOffs);
+    virtual ItemEntity* spawnAtLocation(ItemInstance* itemInstance, float yOffs);
 
-	virtual void awardKillScore(Entity* victim, int score);
+    virtual void awardKillScore(Entity* victim, int score);
 
-	virtual void setEquippedSlot(int slot, int item, int auxValue);
+    virtual void setEquippedSlot(int slot, int item, int auxValue);
 
-	virtual bool save(CompoundTag* entityTag);
-	virtual void saveWithoutId(CompoundTag* entityTag);
-	virtual bool load(CompoundTag* entityTag);
-	virtual SynchedEntityData* getEntityData();
-	virtual const SynchedEntityData* getEntityData() const;
+    virtual bool save(CompoundTag* entityTag);
+    virtual void saveWithoutId(CompoundTag* entityTag);
+    virtual bool load(CompoundTag* entityTag);
+    virtual SynchedEntityData* getEntityData();
+    virtual const SynchedEntityData* getEntityData() const;
 
-	__inline bool isEntityType(int type) { return getEntityTypeId() == type; }
-	virtual int getEntityTypeId() const = 0;
-	virtual int getCreatureBaseType() const { return 0; }
-	virtual EntityRendererId queryEntityRenderer() { return ER_DEFAULT_RENDERER; }
+    __inline bool isEntityType(int type) { return getEntityTypeId() == type; }
+    virtual int getEntityTypeId() const = 0;
+    virtual int getCreatureBaseType() const { return 0; }
+    virtual EntityRendererId queryEntityRenderer() { return ER_DEFAULT_RENDERER; }
 
-	virtual bool isMob() { return false; }
-	// I hate myself
-	virtual bool isItemEntity();
-	// Me 2
-	virtual bool isHangingEntity();
+    virtual bool isMob() { return false; }
+    virtual bool isItemEntity();
+    virtual bool isHangingEntity();
 
-	virtual int getAuxData();
+    virtual int getAuxData();
 
 protected:
-	virtual void setRot(float yRot, float xRot);
-	virtual void setSize(float w, float h);
-	virtual void setPos(EntityPos* pos);
-	virtual void resetPos(bool clearMore);
-	virtual void outOfWorld();
+    virtual void setRot(float yRot, float xRot);
+    virtual void setSize(float w, float h);
+    virtual void setPos(EntityPos* pos);
+    virtual void resetPos(bool clearMore);
+    virtual void outOfWorld();
 
-	virtual void checkFallDamage(float ya, bool onGround);
-	virtual void causeFallDamage(float fallDamage2);
-	virtual void markHurt();
+    virtual void checkFallDamage(float ya, bool onGround);
+    virtual void causeFallDamage(float fallDamage2);
+    virtual void markHurt();
 
-	virtual void burn(int dmg);
-	virtual void lavaHurt();
+    virtual void burn(int dmg);
+    virtual void lavaHurt();
 
-	virtual void readAdditionalSaveData(CompoundTag* tag) = 0;
-	virtual void addAdditonalSaveData(CompoundTag* tag) = 0;
+    virtual void readAdditionalSaveData(CompoundTag* tag) = 0;
+    virtual void addAdditonalSaveData(CompoundTag* tag) = 0;
 
-    virtual void playStepSound( int xt, int yt, int zt, int t );
+    virtual void playStepSound(int64_t xt, int yt, int64_t zt, int t);
+
 public:
-	double x, y, z;
+    double x, y, z;
 
-	int xChunk, yChunk, zChunk;
+    int64_t xChunk, yChunk, zChunk;   // 区块索引（单位：区块），改为 int64_t
 
-	int entityId;
+    int entityId;
 
-	float viewScale;
+    float viewScale;
 
-	Level* level;
-	double xo, yo, zo;
+    Level* level;
+    double xo, yo, zo;
     double xd, yd, zd;
-	float yRot, xRot;
-	float yRotO, xRotO;
+    float yRot, xRot;
+    float yRotO, xRotO;
 
-	AABB bb;
+    AABB bb;
 
-	float heightOffset; // = 0 / 16.0f;
+    float heightOffset;
 
-	float bbWidth;
-	float bbHeight;
+    float bbWidth;
+    float bbHeight;
 
-	float walkDistO;
-	float walkDist;
+    float walkDistO;
+    float walkDist;
 
-	double xOld, yOld, zOld;
-	float ySlideOffset;
-	float footSize;
-	float pushthrough;
+    double xOld, yOld, zOld;
+    float ySlideOffset;
+    float footSize;
+    float pushthrough;
 
-	int tickCount;
-	int invulnerableTime;
-	int airSupply;
-	int onFire;
-	int flameTime;
+    int tickCount;
+    int invulnerableTime;
+    int airSupply;
+    int onFire;
+    int flameTime;
 
-	EntityRendererId entityRendererId;
+    EntityRendererId entityRendererId;
 
-	//Ref<Entity>* ref;
+    float fallDistance;
+    bool blocksBuilding;
+    bool inChunk;
 
-	//   bool hovered = false;
+    bool onGround;
+    bool horizontalCollision, verticalCollision;
+    bool collision;
+    bool hurtMarked;
 
-	//   std::string customTextureUrl;
-	//   std::string customTextureUrl2;
-	//   /*protected*/ bool fireImmune = false;
-
-	float fallDistance;
-	bool blocksBuilding;
-	bool inChunk;
-
-	bool onGround;
-	bool horizontalCollision, verticalCollision;
-	bool collision;
-	bool hurtMarked;
-
-	bool slide;
-	bool removed;
-	bool noPhysics;
-	bool canRemove;
-	bool invisible;
-	bool reallyRemoveIfPlayer;
-protected:
-	static Random sharedRandom;
-	int airCapacity;
-	bool makeStepSound;
-	bool wasInWater;
-	bool fireImmune;
+    bool slide;
+    bool removed;
+    bool noPhysics;
+    bool canRemove;
+    bool invisible;
+    bool reallyRemoveIfPlayer;
 
 protected:
-	bool firstTick;
-	int nextStep;
-	static const int DATA_AIR_SUPPLY_ID = 1;
-	bool isStuckInWeb;
+    static Random sharedRandom;
+    int airCapacity;
+    bool makeStepSound;
+    bool wasInWater;
+    bool fireImmune;
+
+private:
+    bool firstTick;
+    int nextStep;
+    static const int DATA_AIR_SUPPLY_ID = 1;
+    bool isStuckInWeb;
 };
 
-#endif /*NET_MINECRAFT_WORLD_ENTITY__Entity_H__*/
-
+#endif
