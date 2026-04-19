3b97bdacaf7f8f4c252d6ebcd397388281845999
MFSCelebrate_ <19358185571@163.com>
2026-04-19 10:32:41 +0800

    Update RegionFile.h

--- src/world/level/storage/RegionFile.h
+++ src/world/level/storage/RegionFile.h
@@ -4,36 +4,38 @@
 #include <map>
 #include <string>
 #include <cstdint>
+#include <cstdio>
 #include "../../../raknet/BitStream.h"
 
-typedef std::map<int, bool> FreeSectorMap;
-
-class RegionFile
-{
+class RegionFile {
 public:
-    RegionFile(const std::string& basePath);
-    virtual ~RegionFile();
+    static const int CHUNKS_PER_REGION = 32;   // 每个区域 32×32 区块
+    static const int SECTOR_SIZE = 4096;       // 扇区大小 4KB
+
+    RegionFile(const std::string& filePath);
+    ~RegionFile();
 
     bool open();
-    bool readChunk(int64_t x, int64_t z, RakNet::BitStream** destChunkData);
-    bool writeChunk(int64_t x, int64_t z, RakNet::BitStream& chunkData);
-
-private:
-    int  allocateSector(int sectorsNeeded);
-    void readSectorMap();
-    void writeSectorMap();
-    bool write(int sector, RakNet::BitStream& chunkData);
     void close();
 
-    FILE* file;
-    std::string filename;
-    int* offsets;
-    int* emptyChunk;
-    FreeSectorMap sectorFree;
-    bool useOldFormat;
+    bool readChunk(int localX, int localZ, RakNet::BitStream** outData);
+    bool writeChunk(int localX, int localZ, RakNet::BitStream& data);
 
-    // 使用 int64_t 键支持超大坐标
-    std::map<std::pair<int64_t, int64_t>, int> chunkToSector;
+private:
+    struct ChunkInfo {
+        uint32_t sectorStart;   // 起始扇区号（0 表示未使用）
+        uint32_t sizeBytes;     // 数据字节数（不含长度头）
+    };
+
+    bool loadOffsetTable();
+    bool saveOffsetTable();
+    uint32_t allocateSectors(uint32_t sectorsNeeded);
+    void freeSectors(uint32_t startSector, uint32_t sectorCount);
+
+    std::string m_filePath;
+    FILE* m_file;
+    ChunkInfo m_chunks[CHUNKS_PER_REGION][CHUNKS_PER_REGION];
+    std::map<uint32_t, bool> m_freeSectors; // true = 空闲
 };
 
 #endif
