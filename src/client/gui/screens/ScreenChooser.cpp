e711b4288e4d81882a8414b6da118279fac860b3
MFSCelebrate_ <MFSCelebrate@163.com>
2026-04-25 10:32:54 +0800

    Update ScreenChooser.cpp

--- src/client/gui/screens/ScreenChooser.cpp
+++ src/client/gui/screens/ScreenChooser.cpp
@@ -11,6 +11,7 @@
 #include "touch/TouchSelectWorldScreen.h"
 #include "touch/TouchJoinGameScreen.h"
 #include "touch/TouchIngameBlockSelectionScreen.h"
+#include "DebugScreen.h"                  // 新增加
 
 #include "../../Minecraft.h"
 
@@ -18,46 +19,47 @@
 
 Screen* ScreenChooser::createScreen( ScreenId id )
 {
-	Screen* screen = NULL;
+    Screen* screen = NULL;
 
-	// :sob:
-	if (/* _mc->useTouchscreen() */ true) {
-		switch (id) {
-		case SCREEN_STARTMENU:	     screen = new Touch::StartMenuScreen();	break;
-		case SCREEN_SELECTWORLD:     screen = new Touch::SelectWorldScreen();break;
-		case SCREEN_JOINGAME:	     screen = new Touch::JoinGameScreen();	break;
-		case SCREEN_PAUSE:	         screen = new PauseScreen(false); break;
-		case SCREEN_PAUSEPREV:	     screen = new PauseScreen(true);	 break;
-		case SCREEN_BLOCKSELECTION:	 screen = new Touch::IngameBlockSelectionScreen();	break;
-		case SCREEN_JOINBYIP:        screen = new JoinByIPScreen(); break;
-		case SCREEN_CONSOLE:		 screen = new ConsoleScreen(); break;
-		case SCREEN_NONE:
-		default:
-			// Do nothing
-			break;
-		}
-	} else {
-		switch (id) {
-		case SCREEN_STARTMENU:	     screen = new StartMenuScreen();  break;
-		case SCREEN_SELECTWORLD:     screen = new SelectWorldScreen();break;
-		case SCREEN_JOINGAME:	     screen = new JoinGameScreen();   break;
-		case SCREEN_PAUSE:	         screen = new PauseScreen(false); break;
-		case SCREEN_PAUSEPREV:	     screen = new PauseScreen(true);	 break;
-		case SCREEN_BLOCKSELECTION:	 screen = new IngameBlockSelectionScreen();	break;
-		case SCREEN_JOINBYIP:        screen = new JoinByIPScreen(); break;
-		case SCREEN_CONSOLE:		 screen = new ConsoleScreen(); break;
-		case SCREEN_NONE:
-		default:
-			// Do nothing
-			break;
-		}
-	}
-	return screen;
+    // 触摸屏界面
+    if (/* _mc->useTouchscreen() */ true) {
+        switch (id) {
+            case SCREEN_STARTMENU:    screen = new Touch::StartMenuScreen(); break;
+            case SCREEN_SELECTWORLD:  screen = new Touch::SelectWorldScreen();break;
+            case SCREEN_JOINGAME:     screen = new Touch::JoinGameScreen(); break;
+            case SCREEN_PAUSE:        screen = new PauseScreen(false); break;
+            case SCREEN_PAUSEPREV:    screen = new PauseScreen(true); break;
+            case SCREEN_BLOCKSELECTION: screen = new Touch::IngameBlockSelectionScreen(); break;
+            case SCREEN_JOINBYIP:     screen = new JoinByIPScreen(); break;
+            case SCREEN_CONSOLE:      screen = new ConsoleScreen(); break;
+            case SCREEN_DEBUG:        screen = new DebugScreen(_mc); break;   // 新增加
+            case SCREEN_NONE:
+            default:
+                break;
+        }
+    } else {
+        // 键盘/鼠标界面
+        switch (id) {
+            case SCREEN_STARTMENU:    screen = new StartMenuScreen(); break;
+            case SCREEN_SELECTWORLD:  screen = new SelectWorldScreen();break;
+            case SCREEN_JOINGAME:     screen = new JoinGameScreen(); break;
+            case SCREEN_PAUSE:        screen = new PauseScreen(false); break;
+            case SCREEN_PAUSEPREV:    screen = new PauseScreen(true); break;
+            case SCREEN_BLOCKSELECTION: screen = new IngameBlockSelectionScreen(); break;
+            case SCREEN_JOINBYIP:     screen = new JoinByIPScreen(); break;
+            case SCREEN_CONSOLE:      screen = new ConsoleScreen(); break;
+            case SCREEN_DEBUG:        screen = new DebugScreen(_mc); break;   // 新增加
+            case SCREEN_NONE:
+            default:
+                break;
+        }
+    }
+    return screen;
 }
 
 Screen* ScreenChooser::setScreen(ScreenId id)
 {
-	Screen* screen = createScreen(id);
-	_mc->setScreen(screen);
-	return screen;
+    Screen* screen = createScreen(id);
+    _mc->setScreen(screen);
+    return screen;
 }
