8687a047e2696708b652a09dfc7b9adba39a4e10
MFSCelebrate_ <MFSCelebrate@163.com>
2026-04-25 10:44:47 +0800

    Update TouchscreenInput.h

--- src/client/player/input/touchscreen/TouchscreenInput.h
+++ src/client/player/input/touchscreen/TouchscreenInput.h
@@ -1,6 +1,8 @@
 #ifndef NET_MINECRAFT_CLIENT_PLAYER_INPUT_TOUCHSCREEN_TouchscreenInput_H__
 #define NET_MINECRAFT_CLIENT_PLAYER_INPUT_TOUCHSCREEN_TouchscreenInput_H__
 
+//package net.minecraft.client.player;
+
 #include "../IMoveInput.h"
 #include "../../../gui/GuiComponent.h"
 #include "TouchAreaModel.h"
@@ -9,8 +11,11 @@
 class Options;
 class Player;
 class Minecraft;
+class PolygonArea;
 
-class TouchscreenInput_TestFps : public IMoveInput, public GuiComponent
+// @todo: extract a separate MoveInput (-> merge XperiaPlayInput)
+class TouchscreenInput_TestFps:	public IMoveInput,
+								public GuiComponent
 {
 public:
     static const int KEY_UP = 0;
@@ -19,57 +24,57 @@ public:
     static const int KEY_RIGHT = 3;
     static const int KEY_JUMP = 4;
     static const int KEY_SNEAK = 5;
-    static const int KEY_CRAFT = 6;
-    static const int NumKeys = 7;
+	static const int KEY_CRAFT = 6;
+	static const int NumKeys = 7;
 
     TouchscreenInput_TestFps(Minecraft* mc, Options* options);
-    ~TouchscreenInput_TestFps();
+	~TouchscreenInput_TestFps();
 
-    void onConfigChanged(const Config& c);
-    void tick(Player* player);
-    void render(float a);
-    void setKey(int key, bool state);
+	void onConfigChanged(const Config& c);
+
+	void tick(Player* player);
+	void render(float a);
+
+	void setKey(int key, bool state);
     void releaseAllKeys();
 
-    const RectangleArea& getRectangleArea();
+	const RectangleArea& getRectangleArea();
     const RectangleArea& getPauseRectangleArea();
 
 private:
-    void clear();
-    void rebuild();
-    bool isButtonDown(int areaId);
+	void clear();
 
-    RectangleArea _boundingRectangle;
-    bool _keys[NumKeys];
-    Options* _options;
+	RectangleArea _boundingRectangle;
 
-    bool _pressedJump;
-    bool _forward;
-    bool _northJump;
-    bool _renderFlightImage;
+	bool _keys[NumKeys];
+	Options* _options;
 
-    TouchAreaModel _model;
-    Minecraft* _minecraft;
+	bool _pressedJump;
+	bool _forward;
+	bool _northJump;
+	bool _renderFlightImage;
+	TouchAreaModel _model;
+	Minecraft* _minecraft;
 
-    RectangleArea* aLeft;
-    RectangleArea* aRight;
-    RectangleArea* aUp;
-    RectangleArea* aDown;
-    RectangleArea* aPause;
-    RectangleArea* aChat;
-    RectangleArea* aJump;
-    RectangleArea* aUpLeft;
-    RectangleArea* aUpRight;
+	RectangleArea* aLeft;
+	RectangleArea* aRight;
+	RectangleArea* aUp;
+	RectangleArea* aDown;
+	RectangleArea* aPause;
+	RectangleArea* aChat;
+	//RectangleArea* aUpJump;
+	RectangleArea* aJump;
+	RectangleArea* aUpLeft;
+	RectangleArea* aUpRight;
+	bool _pauseIsDown;
 
-    bool _pauseIsDown;
-    RenderChunk _render;
-    bool _allowHeightChange;
-    float _sneakTapTime;
-    bool _buttons[8];
+	RenderChunk _render;
+	bool _allowHeightChange;
+	float _sneakTapTime;
 
-    // 新增调试齿轮按钮
-    RectangleArea* aDebug;
-    static const int AREA_DEBUG = 200;
+	bool _buttons[8];
+	bool isButtonDown(int areaId);
+	void rebuild();
 };
 
-#endif
+#endif /*NET_MINECRAFT_CLIENT_PLAYER_INPUT_TOUCHSCREEN_TouchscreenInput_H__*/
