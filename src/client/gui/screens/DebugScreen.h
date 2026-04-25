2610d5b129cb5c8183a0fad4b10c6a885fe7693e
MFSCelebrate_ <MFSCelebrate@163.com>
2026-04-25 19:11:28 +0800

    Update DebugScreen.h

--- src/client/gui/screens/DebugScreen.h
+++ src/client/gui/screens/DebugScreen.h
@@ -6,29 +6,33 @@
 #include "../../Minecraft.h"
 #include <vector>
 
-class DebugScreen : public Screen {
+class DebugScreen : public Screen
+{
 public:
     DebugScreen(Minecraft* mc);
     virtual ~DebugScreen();
-    virtual void init();
-    virtual void setupPositions();
-    virtual void render(int xm, int ym, float a);
-    virtual bool renderGameBehind() { return true; }
-    virtual bool isInGameScreen() { return false; }
-    virtual bool isPauseScreen() { return false; }
+
+    virtual void init() override;
+    virtual void setupPositions() override;
+    virtual void render(int xm, int ym, float a) override;
+
+    virtual bool renderGameBehind() override { return true; }
+    virtual bool isInGameScreen() override { return false; }
+    virtual bool isPauseScreen() override { return false; }
 
     virtual void tick() override;
-    virtual void mouseClicked(int x, int y, int buttonNum) override;
-    virtual void mouseReleased(int x, int y, int buttonNum) override;
-    virtual void mouseEvent() override;      // 处理移动
 
 protected:
-    virtual void buttonClicked(Button* button);
+    virtual void buttonClicked(Button* button) override;
 
 private:
     void addButton(int id, const std::string& text);
     void executeAction(int id);
     void updateScrollLimits();
+
+    virtual void mouseClicked(int x, int y, int buttonNum) override;
+    virtual void mouseReleased(int x, int y, int buttonNum) override;
+    virtual void mouseEvent() override;
 
     Minecraft* mc;
     std::vector<Button*> debugButtons;
@@ -36,22 +40,31 @@ private:
     float scrollY;
     float maxScroll;
     float lastTouchY;
-    bool  dragging;
-    int   contentHeight;
-    int   viewportHeight;
+    bool dragging;
+    int contentHeight;
+    int viewportHeight;
 
-    static const int BTN_GODMODE    = 0;
-    static const int BTN_GAMEMODE   = 1;
-    static const int BTN_TIME       = 2;
-    static const int BTN_ARMOR      = 3;
-    static const int BTN_HURT_RELOAD= 4;
-    static const int BTN_SPAWNMOB   = 5;
-    static const int BTN_MASSACRE   = 6;
-    static const int BTN_CLEARINV   = 7;
-    static const int BTN_PRERENDER  = 8;
-    static const int BTN_DROPALL    = 9;
-    static const int BTN_SPEEDUP    = 10;
-    static const int BTN_3RDPERSON  = 11;
+    Button* _pressedButton;
+
+    static const int BTN_GODMODE      = 0;
+    static const int BTN_GAMEMODE     = 1;
+    static const int BTN_TIME         = 2;
+    static const int BTN_ARMOR        = 3;
+    static const int BTN_HURT_RELOAD  = 4;
+    static const int BTN_SPAWNMOB     = 5;
+    static const int BTN_MASSACRE     = 6;
+    static const int BTN_CLEARINV     = 7;
+    static const int BTN_PRERENDER    = 8;
+    static const int BTN_DROPALL      = 9;
+    static const int BTN_SPEEDUP      = 10;
+    static const int BTN_3RDPERSON    = 11;
+
+    static const int BTN_NOPVP        = 12;
+    static const int BTN_NOPVM        = 13;
+    static const int BTN_NOMVP        = 14;
+    static const int BTN_IMMUTABLE    = 15;
+    static const int BTN_NAMETAGS     = 16;
+    static const int BTN_PARTICLES    = 17;
 };
 
 #endif
