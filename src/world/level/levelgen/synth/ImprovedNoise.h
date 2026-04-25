b26dac3e07cf319e095a57e476582f6ea6283563
MFSCelebrate_ <MFSCelebrate@163.com>
2026-04-25 20:35:52 +0800

    Update ImprovedNoise.h

--- src/world/level/levelgen/synth/ImprovedNoise.h
+++ src/world/level/levelgen/synth/ImprovedNoise.h
@@ -22,10 +22,10 @@ public:
     const float grad2(int hash, float x, float z);
     const float grad(int hash, float x, float y, float z);
 
-    float getValue(float x, float y);
-    float getValue(float x, float y, float z);
-float getValue(double x, double y);          // 2D double 版本
-float getValue(double x, double y, double z); // 3D double 版本
+    float getValue(float x, float y) const;
+    float getValue(float x, float y, float z) const;
+float getValue(double x, double y) const;          // 2D double 版本
+float getValue(double x, double y, double z) const; // 3D double 版本
 
     void add(float* buffer, float _x, float _y, float _z, int xSize, int ySize, int zSize, float xs, float ys, float zs, float pow);
 
