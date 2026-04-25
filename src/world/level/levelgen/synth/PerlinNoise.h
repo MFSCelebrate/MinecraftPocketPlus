36bcdacbb5cb7a928ada9c2f3be73119aa117f39
MFSCelebrate_ <MFSCelebrate@163.com>
2026-04-25 20:43:02 +0800

    Update PerlinNoise.h

--- src/world/level/levelgen/synth/PerlinNoise.h
+++ src/world/level/levelgen/synth/PerlinNoise.h
@@ -17,7 +17,18 @@ public:
 
     float getValue(float x, float y) const;
     float getValue(float x, float y, float z) const;
-float getValue(double x, double y, double z) const;
+    float getValue(double x, double y, double z) const;
+
+    // 新增：非 const 版本，满足基类 Synth 纯虚函数
+    inline float getValue(float x, float y) override {
+        return static_cast<const PerlinNoise*>(this)->getValue(x, y);
+    }
+    inline float getValue(float x, float y, float z) {
+        return static_cast<const PerlinNoise*>(this)->getValue(x, y, z);
+    }
+    inline float getValue(double x, double y, double z) {
+        return static_cast<const PerlinNoise*>(this)->getValue(x, y, z);
+	}
 
 	//float[] getRegion(float[] buffer, float x, float y, float z, int xSize, int ySize, int zSize, float xScale, float yScale, float zScale) {
     float* getRegion(float* buffer, float x, float y, float z, int xSize, int ySize, int zSize, float xScale, float yScale, float zScale);
