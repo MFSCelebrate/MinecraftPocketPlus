9d166effa4ddf7fb84e80b6db7fcec5985ed5baf
MFSCelebrate_ <MFSCelebrate@163.com>
2026-04-25 11:33:55 +0800

    Update DebugScreen.h

--- src/client/gui/screens/DebugScreen.h
+++ src/client/gui/screens/DebugScreen.h
@@ -17,27 +17,41 @@ public:
     virtual bool isInGameScreen() { return false; }
     virtual bool isPauseScreen() { return false; }
 
+    virtual void tick() override;
+    virtual void mouseClicked(int x, int y, int buttonNum) override;
+    virtual void mouseReleased(int x, int y, int buttonNum) override;
+    virtual void mouseEvent() override;      // 处理移动
+
 protected:
     virtual void buttonClicked(Button* button);
 
 private:
     void addButton(int id, const std::string& text);
     void executeAction(int id);
+    void updateScrollLimits();
 
     Minecraft* mc;
     std::vector<Button*> debugButtons;
-    static const int BTN_GODMODE = 0;
-    static const int BTN_GAMEMODE = 1;
-    static const int BTN_TIME = 2;
-    static const int BTN_ARMOR = 3;
-    static const int BTN_HURT_RELOAD = 4;
-    static const int BTN_SPAWNMOB = 5;
-    static const int BTN_MASSACRE = 6;
-    static const int BTN_CLEARINV = 7;
-    static const int BTN_PRERENDER = 8;
-    static const int BTN_DROPALL = 9;
-    static const int BTN_SPEEDUP = 10;
-    static const int BTN_3RDPERSON = 11;
+
+    float scrollY;
+    float maxScroll;
+    float lastTouchY;
+    bool  dragging;
+    int   contentHeight;
+    int   viewportHeight;
+
+    static const int BTN_GODMODE    = 0;
+    static const int BTN_GAMEMODE   = 1;
+    static const int BTN_TIME       = 2;
+    static const int BTN_ARMOR      = 3;
+    static const int BTN_HURT_RELOAD= 4;
+    static const int BTN_SPAWNMOB   = 5;
+    static const int BTN_MASSACRE   = 6;
+    static const int BTN_CLEARINV   = 7;
+    static const int BTN_PRERENDER  = 8;
+    static const int BTN_DROPALL    = 9;
+    static const int BTN_SPEEDUP    = 10;
+    static const int BTN_3RDPERSON  = 11;
 };
 
 #endif
