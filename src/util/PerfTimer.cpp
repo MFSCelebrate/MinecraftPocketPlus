aae7e06d22cd805724a3a2cbb7545699e873d75d
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 19:21:26 +0800

    Update PerfTimer.cpp

--- src/util/PerfTimer.cpp
+++ src/util/PerfTimer.cpp
@@ -3,10 +3,7 @@
 #include <algorithm>
 
 bool PerfTimer::enabled = false;
-int  PerfTimer::s_sampleInterval = 4;   // 每4帧记录一次数据
-int  PerfTimer::s_frameCounter  = 0;
-std::vector<PerfTimer::ResultField> PerfTimer::s_cachedLog;
-std::string PerfTimer::s_cachedPath;
+int  PerfTimer::s_frameCounter = 0;
 
 std::vector<std::string> PerfTimer::paths;
 std::vector<float> PerfTimer::startTimes;
@@ -19,8 +16,6 @@ void PerfTimer::tickFrame() {
 
 void PerfTimer::reset() {
     times.clear();
-    s_cachedLog.clear();
-    s_cachedPath.clear();
 }
 
 void PerfTimer::push( const std::string& name ) {
@@ -51,23 +46,20 @@ void PerfTimer::popPush( const std::string& name ) {
     push(name);
 }
 
-std::vector<PerfTimer::ResultField> PerfTimer::getLog(const std::string& rawPath, bool forceUpdate) {
+std::vector<PerfTimer::ResultField> PerfTimer::getLog(const std::string& rawPath, bool /*forceUpdate*/) {
     if (!enabled) return std::vector<ResultField>();
-
-    // 非强制更新，且每4帧才重新计算，其余返回缓存
-    if (!forceUpdate && (s_frameCounter % 4 != 0) && (s_cachedPath == rawPath) && !s_cachedLog.empty()) {
-        return s_cachedLog;
-    }
 
     std::string path = rawPath;
     TimeMap::const_iterator itRoot = times.find("root");
     float globalTime = (itRoot != times.end())? itRoot->second : 0;
     TimeMap::const_iterator itPath = times.find(path);
     float totalTime2 = (itRoot != times.end())? itRoot->second : -1;
+
     std::vector<ResultField> result;
 
     if (path.length() > 0) path += ".";
     float totalTime = 0;
+
     for (TimeMap::const_iterator cit = times.begin(); cit != times.end(); ++cit) {
         const std::string& key = cit->first;
         const float& time = cit->second;
@@ -99,8 +91,5 @@ std::vector<PerfTimer::ResultField> PerfTimer::getLog(const std::string& rawPath
 
     std::sort(result.begin(), result.end());
     result.insert(result.begin(), ResultField(rawPath, 100, totalTime * 100.0f / globalTime));
-
-    s_cachedLog = result;
-    s_cachedPath = rawPath;
     return result;
 }
