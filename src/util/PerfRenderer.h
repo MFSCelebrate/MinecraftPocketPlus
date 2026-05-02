7518bf45a91dc1773b4391130bd17fedbabf048f
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 23:49:18 +0800

    Update PerfRenderer.h

--- src/util/PerfRenderer.h
+++ src/util/PerfRenderer.h
@@ -4,11 +4,6 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <atomic>
-
 #include "PerfTimer.h"
 
 class Minecraft;
@@ -17,14 +12,10 @@ class Font;
 class PerfRenderer {
 public:
     PerfRenderer( Minecraft* mc, Font* font );
-    ~PerfRenderer();
-
     void debugFpsMeterKeyPress(int key);
     void renderFpsMeter(float tickTime);
 
 private:
-    void workerLoop();
-
     std::string toPercentString(float percentage);
 
     Minecraft* _mc;
@@ -35,22 +26,6 @@ private:
     std::vector<float> tickTimes;
     int frameTimePos;
     float lastTimer;
-
-    // 后台线程
-    std::thread _worker;
-    std::mutex  _mutex;
-    std::condition_variable _cv;
-    std::atomic<bool> _workerRunning{false};
-    std::atomic<bool> _needsUpdate{false};
-
-    // 共享数据
-    std::vector<PerfTimer::ResultField> _latestList;
-    PerfTimer::ResultField _latestNode;
-
-    // 后备数据（防消失）
-    std::vector<PerfTimer::ResultField> _lastValidList;
-    PerfTimer::ResultField _lastValidNode{"", 0, 0};
-    bool _hasValidData = false;
 };
 
 #endif
