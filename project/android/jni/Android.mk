f47b195a1b0246228762cc770f325405b574c787
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 22:30:56 +0800

    Update Android.mk

--- project/android/jni/Android.mk
+++ project/android/jni/Android.mk
@@ -271,7 +271,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../src
 #LOCAL_CFLAGS := -DDEMO_MODE -DGLDEBUG $(LOCAL_CFLAGS)
 #LOCAL_CFLAGS := -DGLDEBUG $(LOCAL_CFLAGS)
 
-LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM -lOpenSLES -lpthread
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM -lOpenSLES
 LOCAL_STATIC_LIBRARIES := android_native_app_glue RakNet
 
 #LOCAL_CPP_FEATURES := exceptions
