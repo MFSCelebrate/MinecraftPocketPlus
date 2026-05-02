20570e92e5a2e335d29608b75aa9ed88c5920a99
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 16:15:34 +0800

    Update PerfTimer.h

--- src/util/PerfTimer.h
+++ src/util/PerfTimer.h
@@ -1,4 +1,3 @@
-// PerfTimer.h
 #ifndef NET_UTIL__PerfTimer_H__
 #define NET_UTIL__PerfTimer_H__
 
@@ -6,13 +5,10 @@
 #include <vector>
 #include "StringUtils.h"
 
-// 剖析器总开关，由 PerfRenderer 控制
-extern bool gPerfTimerEnabled;
-
 #ifdef PROFILER
-    #define TIMER_PUSH(x)       do { if (gPerfTimerEnabled) PerfTimer::push(x); } while(0)
-    #define TIMER_POP()         do { if (gPerfTimerEnabled) PerfTimer::pop(); } while(0)
-    #define TIMER_POP_PUSH(x)   do { if (gPerfTimerEnabled) { PerfTimer::pop(); PerfTimer::push(x); } } while(0)
+    #define TIMER_PUSH(x)       PerfTimer::push(x)
+    #define TIMER_POP()         PerfTimer::pop()
+    #define TIMER_POP_PUSH(x)   PerfTimer::popPush(x)
 #else
     #define TIMER_PUSH(x)       ((void*)0)
     #define TIMER_POP()         ((void*)0)
@@ -28,17 +24,13 @@ public:
         float percentage;
         float globalPercentage;
         std::string name;
-
         ResultField(const std::string& name, float percentage, float globalPercentage)
             : name(name), percentage(percentage), globalPercentage(globalPercentage) {}
-
         bool operator<(const ResultField& rf) const {
             if (percentage != rf.percentage) return percentage > rf.percentage;
             return name > rf.name;
         }
-        int getColor() const {
-            return (Util::hashCode(name) & 0xaaaaaa) + 0x444444;
-        }
+        int getColor() const { return (Util::hashCode(name) & 0xaaaaaa) + 0x444444; }
     };
 
     static void reset();
@@ -47,6 +39,7 @@ public:
     static void popPush(const std::string& name);
     static std::vector<ResultField> getLog(const std::string& path);
 
+    static bool enabled;   // 始终为 true
 private:
     static std::vector<std::string> paths;
     static std::vector<float> startTimes;
