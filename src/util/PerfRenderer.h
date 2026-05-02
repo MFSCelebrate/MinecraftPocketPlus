cb7727fe7f2c70316ca60420b7137458c3b17436
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 19:54:33 +0800

    Update PerfRenderer.h

--- src/util/PerfRenderer.h
+++ src/util/PerfRenderer.h
@@ -4,31 +4,50 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+#include "PerfTimer.h"
 
 class Minecraft;
 class Font;
 
-//package net.minecraft.client;
-class PerfRenderer
-{
+class PerfRenderer {
 public:
-    PerfRenderer( Minecraft* mc, Font* font);
+    PerfRenderer( Minecraft* mc, Font* font );
+    ~PerfRenderer();
 
     void debugFpsMeterKeyPress(int key);
-
     void renderFpsMeter(float tickTime);
 
 private:
+    void startWorkerThread();
+    void stopWorkerThread();
+    void workerLoop();
+
     std::string toPercentString(float percentage);
 
-	Minecraft*	_mc;
-    Font*		_font;
+    Minecraft* _mc;
+    Font*      _font;
     std::string _debugPath;
 
-	std::vector<float> frameTimes;
-	std::vector<float> tickTimes;
-	int frameTimePos;
-	float lastTimer;
+    std::vector<float> frameTimes;
+    std::vector<float> tickTimes;
+    int frameTimePos;
+    float lastTimer;
+
+    // 后台线程相关
+    std::thread         _workerThread;
+    std::mutex          _dataMutex;
+    std::condition_variable _cv;
+    std::atomic<bool>   _workerRunning{false};
+    bool                _needsUpdate{true};
+
+    // 线程间共享的最新数据
+    std::vector<PerfTimer::ResultField> _latestList;
+    PerfTimer::ResultField              _latestNode{"", 0.0f, 0.0f};
 };
 
-#endif /*NET_UTIL__PerfRenderer_H__*/
+#endif
