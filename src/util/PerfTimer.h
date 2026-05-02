5f3809012d2e055803563b586d4104f0f79e636a
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 18:24:52 +0800

    Update PerfTimer.h

--- src/util/PerfTimer.h
+++ src/util/PerfTimer.h
@@ -6,57 +6,71 @@
 #include "StringUtils.h"
 
 #ifdef PROFILER
-    #define TIMER_PUSH(x)       PerfTimer::push(x)
-    #define TIMER_POP()         PerfTimer::pop()
-    #define TIMER_POP_PUSH(x)   PerfTimer::popPush(x)
+    #define TIMER_PUSH(x) do { \
+        if (PerfTimer::enabled && (PerfTimer::s_frameCounter % PerfTimer::s_sampleInterval == 0)) \
+            PerfTimer::push(x); \
+    } while(0)
+
+    #define TIMER_POP() do { \
+        if (PerfTimer::enabled && (PerfTimer::s_frameCounter % PerfTimer::s_sampleInterval == 0)) \
+            PerfTimer::pop(); \
+    } while(0)
+
+    #define TIMER_POP_PUSH(x) do { \
+        if (PerfTimer::enabled && (PerfTimer::s_frameCounter % PerfTimer::s_sampleInterval == 0)) \
+            PerfTimer::popPush(x); \
+    } while(0)
 #else
     #define TIMER_PUSH(x)       ((void*)0)
     #define TIMER_POP()         ((void*)0)
     #define TIMER_POP_PUSH(x)   ((void*)0)
 #endif
+
 class PerfTimer
 {
-	typedef std::map<std::string, float> TimeMap;
+    typedef std::map<std::string, float> TimeMap;
 public:
     class ResultField {
-	public:
+    public:
         float percentage;
         float globalPercentage;
         std::string name;
 
         ResultField(const std::string& name, float percentage, float globalPercentage)
-		:	name(name),
-			percentage(percentage),
-			globalPercentage(globalPercentage)
-		{}
+            : name(name), percentage(percentage), globalPercentage(globalPercentage) {}
 
         bool operator<(const ResultField& rf) const {
-			if (percentage != rf.percentage)
-				return percentage > rf.percentage;
-			return name > rf.name;
+            if (percentage != rf.percentage) return percentage > rf.percentage;
+            return name > rf.name;
         }
-
         int getColor() const {
             return (Util::hashCode(name) & 0xaaaaaa) + 0x444444;
         }
     };
 
     static void reset();
-
     static void push(const std::string& name);
     static void pop();
-    static std::vector<ResultField> getLog(const std::string& path, bool forceUpdate = false);
     static void popPush(const std::string& name);
 
+    // 新的 getLog 签名：forceUpdate 强制忽略缓存
+    static std::vector<ResultField> getLog(const std::string& path, bool forceUpdate = false);
+
+    // 每帧调用一次，推进全局帧计数
+    static void tickFrame();
+
     static bool enabled;
+    static int  s_sampleInterval;   // 采样间隔，默认 4（每4帧记录一次）
+
 private:
+    static int  s_frameCounter;     // 统一帧计数器
+    static std::vector<ResultField> s_cachedLog;
+    static std::string s_cachedPath;
+
     static std::vector<std::string> paths;
     static std::vector<float> startTimes;
     static std::string path;
     static TimeMap times;
-    static int s_frameCounter;              // 调用计数器
-    static std::vector<ResultField> s_cachedLog; // 缓存结果
-    static std::string s_cachedPath;        // 缓存对应的路径
 };
 
-#endif /*NET_UTIL__PerfTimer_H__*/
+#endif
