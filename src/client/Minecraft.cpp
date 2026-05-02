91c5aef5593f2c4ab9c0086934389b5b5c450a95
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-03 01:59:45 +0800

    Update Minecraft.cpp

--- src/client/Minecraft.cpp
+++ src/client/Minecraft.cpp
@@ -593,6 +593,7 @@ void Minecraft::tick(int nTick, int maxTick) {
 	//      Most likely keyboard and mouse could/should be reset here as well.
 	Multitouch::reset();
 #endif
+	TIMER_POP();
 }
 
 class InputRAII {
