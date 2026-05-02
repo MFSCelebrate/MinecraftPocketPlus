de54061ded47f6f55992794ecc7752581b449851
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 18:06:49 +0800

    Update PerfTimer.cpp

--- src/util/PerfTimer.cpp
+++ src/util/PerfTimer.cpp
@@ -6,6 +6,10 @@
 bool
 	PerfTimer::enabled = false;
 
+int PerfTimer::s_frameCounter = 0;
+std::vector<PerfTimer::ResultField> PerfTimer::s_cachedLog;
+std::string PerfTimer::s_cachedPath;
+
 std::vector<std::string>
 	PerfTimer::paths;
 
@@ -71,53 +75,64 @@ void PerfTimer::popPush( const std::string& name )
 }
 
 /*static*/
-std::vector<PerfTimer::ResultField> PerfTimer::getLog(const std::string& rawPath) {
-	if (!enabled) return std::vector<ResultField>();
+std::vector<PerfTimer::ResultField> PerfTimer::getLog(const std::string& rawPath, bool forceUpdate) {
+    if (!enabled) return std::vector<ResultField>();
 
-	std::string path = rawPath;
+    s_frameCounter++;
 
-	TimeMap::const_iterator itRoot = times.find("root");
-	float globalTime = (itRoot != times.end())? itRoot->second : 0;
+    // 非强制更新，且每4帧才重新计算，其余返回缓存
+    if (!forceUpdate && (s_frameCounter % 4 != 0) && (s_cachedPath == rawPath) && !s_cachedLog.empty()) {
+        return s_cachedLog;
+    }
 
-	TimeMap::const_iterator itPath = times.find(path);
-	float totalTime2 = (itRoot != times.end())? itRoot->second : -1;
+    // ---------- 以下为原始计算逻辑（完全不变） ----------
+    std::string path = rawPath;
 
-	std::vector<ResultField> result;
+    TimeMap::const_iterator itRoot = times.find("root");
+    float globalTime = (itRoot != times.end())? itRoot->second : 0;
 
-	if (path.length() > 0) path += ".";
-	float totalTime = 0;
+    TimeMap::const_iterator itPath = times.find(path);
+    float totalTime2 = (itRoot != times.end())? itRoot->second : -1;
 
-	for (TimeMap::const_iterator cit = times.begin(); cit != times.end(); ++cit) {
-		const std::string& key = cit->first;
-		const float& time = cit->second;
-		if (key.length() > path.length() && Util::startsWith(key, path) && key.find(".", path.length() + 1) == std::string::npos) {
-			totalTime += time;
-		}
-	}
+    std::vector<ResultField> result;
 
-	float oldTime = totalTime;
-	if (totalTime < totalTime2) totalTime = totalTime2;
-	if (globalTime < totalTime) globalTime = totalTime;
+    if (path.length() > 0) path += ".";
+    float totalTime = 0;
 
-	for (TimeMap::const_iterator cit = times.begin(); cit != times.end(); ++cit) {
-		const std::string& key = cit->first;
-		//const float& time = cit->second;
-		if (key.length() > path.length() && Util::startsWith(key, path) && key.find(".", path.length() + 1) == std::string::npos) {
-			float time = times.find(key)->second;
-			float timePercentage = time * 100.0f / totalTime;
-			float globalPercentage = time * 100.0f / globalTime;
-			std::string name = key.substr(path.length());
-			result.push_back(ResultField(name, timePercentage, globalPercentage));
-		}
-	}
+    for (TimeMap::const_iterator cit = times.begin(); cit != times.end(); ++cit) {
+        const std::string& key = cit->first;
+        const float& time = cit->second;
+        if (key.length() > path.length() && Util::startsWith(key, path) && key.find(".", path.length() + 1) == std::string::npos) {
+            totalTime += time;
+        }
+    }
 
-	for (TimeMap::iterator it = times.begin(); it != times.end(); ++it)
-		it->second *= 0.999f;
+    float oldTime = totalTime;
+    if (totalTime < totalTime2) totalTime = totalTime2;
+    if (globalTime < totalTime) globalTime = totalTime;
 
-	if (totalTime > oldTime)
-		result.push_back(ResultField("unspecified", (totalTime - oldTime) * 100.0f / totalTime, (totalTime - oldTime) * 100.0f / globalTime));
+    for (TimeMap::const_iterator cit = times.begin(); cit != times.end(); ++cit) {
+        const std::string& key = cit->first;
+        if (key.length() > path.length() && Util::startsWith(key, path) && key.find(".", path.length() + 1) == std::string::npos) {
+            float time = times.find(key)->second;
+            float timePercentage = time * 100.0f / totalTime;
+            float globalPercentage = time * 100.0f / globalTime;
+            std::string name = key.substr(path.length());
+            result.push_back(ResultField(name, timePercentage, globalPercentage));
+        }
+    }
 
-	std::sort(result.begin(), result.end());
-	result.insert(result.begin(), ResultField(rawPath, 100, totalTime * 100.0f / globalTime));
-	return result;
+    for (TimeMap::iterator it = times.begin(); it != times.end(); ++it)
+        it->second *= 0.999f;
+
+    if (totalTime > oldTime)
+        result.push_back(ResultField("unspecified", (totalTime - oldTime) * 100.0f / totalTime, (totalTime - oldTime) * 100.0f / globalTime));
+
+    std::sort(result.begin(), result.end());
+    result.insert(result.begin(), ResultField(rawPath, 100, totalTime * 100.0f / globalTime));
+
+    // 更新缓存
+    s_cachedLog = result;
+    s_cachedPath = rawPath;
+    return result;
 }
