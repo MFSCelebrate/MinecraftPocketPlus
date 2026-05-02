77e6fdf7437ddad9bf7c48be133620bc97633845
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 16:47:10 +0800

    Back PRh Vanilla

--- src/util/PerfRenderer.h
+++ src/util/PerfRenderer.h
@@ -4,33 +4,31 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include "PerfTimer.h"
 
 class Minecraft;
 class Font;
 
-class PerfRenderer {
+//package net.minecraft.client;
+class PerfRenderer
+{
 public:
-    PerfRenderer( Minecraft* mc, Font* font );
+    PerfRenderer( Minecraft* mc, Font* font);
+
     void debugFpsMeterKeyPress(int key);
+
     void renderFpsMeter(float tickTime);
-    void navigateBack();
-    bool isPieVisible() const { return m_pieVisible; }
 
 private:
-    void togglePie();
     std::string toPercentString(float percentage);
 
-    Minecraft* _mc;
-    Font*      _font;
+	Minecraft*	_mc;
+    Font*		_font;
     std::string _debugPath;
 
-    std::vector<float> frameTimes;
-    std::vector<float> tickTimes;
-    int frameTimePos;
-    float lastTimer;
-
-    bool m_pieVisible = false;
+	std::vector<float> frameTimes;
+	std::vector<float> tickTimes;
+	int frameTimePos;
+	float lastTimer;
 };
 
-#endif
+#endif /*NET_UTIL__PerfRenderer_H__*/
