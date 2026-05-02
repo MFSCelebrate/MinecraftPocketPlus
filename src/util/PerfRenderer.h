05ee0175218a09f8d144200e10e5ee42c51303d7
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-02 20:31:11 +0800

    Update PerfRenderer.h

--- src/util/PerfRenderer.h
+++ src/util/PerfRenderer.h
@@ -4,11 +4,6 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <atomic>
-
 #include "PerfTimer.h"
 
 class Minecraft;
@@ -17,16 +12,11 @@ class Font;
 class PerfRenderer {
 public:
     PerfRenderer( Minecraft* mc, Font* font );
-    ~PerfRenderer();
 
     void debugFpsMeterKeyPress(int key);
     void renderFpsMeter(float tickTime);
 
 private:
-    void startWorkerThread();
-    void stopWorkerThread();
-    void workerLoop();
-
     std::string toPercentString(float percentage);
 
     Minecraft* _mc;
@@ -37,17 +27,6 @@ private:
     std::vector<float> tickTimes;
     int frameTimePos;
     float lastTimer;
-
-    // 后台线程相关
-    std::thread         _workerThread;
-    std::mutex          _dataMutex;
-    std::condition_variable _cv;
-    std::atomic<bool>   _workerRunning{false};
-    bool                _needsUpdate{true};
-
-    // 线程间共享的最新数据
-    std::vector<PerfTimer::ResultField> _latestList;
-    PerfTimer::ResultField              _latestNode{"", 0.0f, 0.0f};
 };
 
 #endif
