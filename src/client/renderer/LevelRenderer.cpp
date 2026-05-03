9ffd6058d7efc187fe26bcef40150286d94e9778
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-03 12:04:39 +0800

    开发版 / 更新 LevelRenderer.cpp 增加Lod渲染
    优化玩家在大视距情况下的帧数，半径128格内正常渲染高精度方块，128格以后使用低质量渲染

--- src/client/renderer/LevelRenderer.cpp
+++ src/client/renderer/LevelRenderer.cpp
@@ -570,7 +570,6 @@ void LevelRenderer::render(const AABB& b) const
 
 int LevelRenderer::renderChunks(int from, int to, int layer, float alpha)
 {
-	
     _renderChunks.clear();
     int count = 0;
     for (int i = from; i < to; i++) {
@@ -583,6 +582,12 @@ int LevelRenderer::renderChunks(int from, int to, int layer, float alpha)
         }
 
         if (!sortedChunks[i]->empty[layer] && sortedChunks[i]->visible && sortedChunks[i]->occlusion_visible) {
+            // ★ LOD：距离玩家超过 128 格时，只绘制不透明固体层 (layer 0)
+            if (layer != 0) {
+                float dSqr = sortedChunks[i]->distanceToSqr(player);
+                if (dSqr > 128.0f * 128.0f) continue;   // 跳过透明层 (layer 1) 和水面 (layer 2)
+            }
+
             int list = sortedChunks[i]->getList(layer);
             if (list >= 0) {
                 _renderChunks.push_back(sortedChunks[i]);
@@ -602,16 +607,15 @@ int LevelRenderer::renderChunks(int from, int to, int layer, float alpha)
 
     for (unsigned int i = 0; i < _renderChunks.size(); ++i) {
         Chunk* chunk = _renderChunks[i];
-        #ifdef USE_VBO
-            renderList.addR(chunk->getRenderChunk(layer));
-        #else
-            renderList.add(chunk->getList(layer));
-        #endif
+#ifdef USE_VBO
+        renderList.addR(chunk->getRenderChunk(layer));
+#else
+        renderList.add(chunk->getList(layer));
+#endif
         renderList.next();
     }
 
     renderSameAsLast(layer, alpha);
-	
     return count;
 }
 
