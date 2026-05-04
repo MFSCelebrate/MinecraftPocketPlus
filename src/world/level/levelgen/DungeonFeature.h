ef0d01d447f28ae53ee45b4801772b38b208620e
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-04 10:22:29 +0800

    Update DungeonFeature.h

--- src/world/level/levelgen/DungeonFeature.h
+++ src/world/level/levelgen/DungeonFeature.h
@@ -4,13 +4,13 @@
 #include "LargeFeature.h"
 
 class DungeonFeature: public LargeFeature {
+public:
+    virtual void addFeature(Level* level, int x, int z, int xOffs, int zOffs,
+                            unsigned char* blocks, int blocksSize) override;
 protected:
     void addRoom(int xOffs, int zOffs, unsigned char* blocks, float xRoom, float yRoom, float zRoom);
     void addTunnel(int xOffs, int zOffs, unsigned char* blocks, float xCave, float yCave, float zCave,
                    float thickness, float yRot, float xRot, int step, int dist, float yScale);
-
-    virtual void addFeature(Level* level, int x, int z, int xOffs, int zOffs,
-                            unsigned char* blocks, int blocksSize) override;
 };
 
 #endif
