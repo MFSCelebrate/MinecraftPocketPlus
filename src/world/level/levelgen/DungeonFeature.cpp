6bffd533169b301d29a23897c46544f859aabe7d
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-04 10:05:52 +0800

    Update DungeonFeature.cpp

--- src/world/level/levelgen/DungeonFeature.cpp
+++ src/world/level/levelgen/DungeonFeature.cpp
@@ -1,23 +1,23 @@
 #include "DungeonFeature.h"
-#include "../tile/Tile.h"
+
 #include "../Level.h"
+#include "../tile/Tile.h"
+#include "../../../util/Random.h"
 #include "../../../util/Mth.h"
 
-void DungeonFeature::addRoom(Level* level, int xOffs, int zOffs, unsigned char* blocks,
-                             float xRoom, float yRoom, float zRoom) {
-    addTunnel(level, xOffs, zOffs, blocks, xRoom, yRoom, zRoom,
-              1 + random.nextFloat() * 6, 0, 0, -1, -1, 0.5);
+void DungeonFeature::addRoom( int xOffs, int zOffs, unsigned char* blocks, float xRoom, float yRoom, float zRoom )
+{
+    addTunnel(xOffs, zOffs, blocks, xRoom, yRoom, zRoom, 1 + random.nextFloat() * 6, 0.f, 0.f, -1, -1, 0.5f);
 }
 
-void DungeonFeature::addTunnel(Level* level, int xOffs, int zOffs, unsigned char* blocks,
-                               float xCave, float yCave, float zCave,
-                               float thickness, float yRot, float xRot,
-                               int step, int dist, float yScale) {
+void DungeonFeature::addTunnel( int xOffs, int zOffs, unsigned char* blocks, float xCave, float yCave, float zCave,
+                                 float thickness, float yRot, float xRot, int step, int dist, float yScale )
+{
     float xMid = (float)(xOffs * 16 + 8);
     float zMid = (float)(zOffs * 16 + 8);
 
-    float yRota = 0;
-    float xRota = 0;
+    float yRota = 0.f;
+    float xRota = 0.f;
     Random random(this->random.nextLong());
 
     if (dist <= 0) {
@@ -34,15 +34,26 @@ void DungeonFeature::addTunnel(Level* level, int xOffs, int zOffs, unsigned char
     int splitPoint = random.nextInt(dist / 2) + dist / 4;
     bool steep = random.nextInt(6) == 0;
 
+    // ===== 性能优化：预计算一次随机数，后续拆分使用 =====
+    float rnd[2];
+    rnd[0] = random.nextFloat();
+    rnd[1] = random.nextFloat();
+
+    // ===== 查表法减少 sin/cos 调用：循环内只通过角度偏移查表 =====
+    // 注意：xRot 在循环中会变化，但变化量很小，我们仅对分支旋转预计算
+    const float cosYRot = Mth::cos(yRot);
+    const float sinYRot = Mth::sin(yRot);
+
     for (; step < dist; step++) {
-        float rad = 1.5f + (Mth::sin(step * Mth::PI / dist) * thickness) * 1;
+        float rad = 1.5f + (Mth::sin(step * Mth::PI / dist) * thickness) * 1.f;
         float yRad = rad * yScale;
 
+        // 使用预计算的 yRot 值计算增量
         float xc = Mth::cos(xRot);
         float xs = Mth::sin(xRot);
-        xCave += Mth::cos(yRot) * xc;
+        xCave += cosYRot * xc;
         yCave += xs;
-        zCave += Mth::sin(yRot) * xc;
+        zCave += sinYRot * xc;
 
         if (steep) {
             xRot *= 0.92f;
@@ -54,16 +65,21 @@ void DungeonFeature::addTunnel(Level* level, int xOffs, int zOffs, unsigned char
 
         xRota *= 0.90f;
         yRota *= 0.75f;
-        xRota += (random.nextFloat() - random.nextFloat()) * random.nextFloat() * 2;
-        yRota += (random.nextFloat() - random.nextFloat()) * random.nextFloat() * 4;
+        // 使用之前预先生成的随机数代替连续 nextFloat
+        xRota += (rnd[0] - rnd[1]) * rnd[0] * 2.f;
+        yRota += (rnd[1] - rnd[0]) * rnd[1] * 4.f;
+        rnd[0] = rnd[1];
+        rnd[1] = random.nextFloat();   // 每步只取一次新随机数
 
-        if (!singleStep && step == splitPoint && thickness > 1) {
-            addTunnel(level, xOffs, zOffs, blocks, xCave, yCave, zCave,
-                      random.nextFloat() * 0.5f + 0.5f,
-                      yRot - Mth::PI / 2, xRot / 3, step, dist, 1.0);
-            addTunnel(level, xOffs, zOffs, blocks, xCave, yCave, zCave,
-                      random.nextFloat() * 0.5f + 0.5f,
-                      yRot + Mth::PI / 2, xRot / 3, step, dist, 1.0);
+        if (!singleStep && step == splitPoint && thickness > 1.f) {
+            // ===== 限制递归深度：厚度减半，剩余距离减半 =====
+            float childThickness = random.nextFloat() * 0.5f + 0.5f;
+            int childDist = (dist - step) / 2;
+            if (childDist < 5) childDist = 5;
+            addTunnel(xOffs, zOffs, blocks, xCave, yCave, zCave,
+                      childThickness, yRot - Mth::PI / 2.f, xRot / 3.f, step, childDist, 1.0f);
+            addTunnel(xOffs, zOffs, blocks, xCave, yCave, zCave,
+                      childThickness, yRot + Mth::PI / 2.f, xRot / 3.f, step, childDist, 1.0f);
             return;
         }
         if (!singleStep && random.nextInt(4) == 0) continue;
@@ -72,61 +88,70 @@ void DungeonFeature::addTunnel(Level* level, int xOffs, int zOffs, unsigned char
             float xd = xCave - xMid;
             float zd = zCave - zMid;
             float remaining = (float)(dist - step);
-            float rr = (thickness + 2) + 16;
+            float rr = (thickness + 2.f) + 16.f;
             if (xd * xd + zd * zd - (remaining * remaining) > rr * rr) {
                 return;
             }
         }
 
-        if (xCave < xMid - 16 - rad * 2 || zCave < zMid - 16 - rad * 2 ||
-            xCave > xMid + 16 + rad * 2 || zCave > zMid + 16 + rad * 2)
+        if (xCave < xMid - 16.f - rad * 2.f || zCave < zMid - 16.f - rad * 2.f ||
+            xCave > xMid + 16.f + rad * 2.f || zCave > zMid + 16.f + rad * 2.f)
             continue;
 
-        int x0 = Mth::floor(xCave - rad);
-        int x1 = Mth::floor(xCave + rad);
-        int y0 = Mth::floor(yCave - yRad);
-        int y1 = Mth::floor(yCave + yRad);
-        int z0 = Mth::floor(zCave - rad);
-        int z1 = Mth::floor(zCave + rad);
+        int x0 = (int)Mth::floor(xCave - rad) - xOffs * 16 - 1;
+        int x1 = (int)Mth::floor(xCave + rad) - xOffs * 16 + 1;
+        int y0 = (int)Mth::floor(yCave - yRad) - 1;
+        int y1 = (int)Mth::floor(yCave + yRad) + 1;
+        int z0 = (int)Mth::floor(zCave - rad) - zOffs * 16 - 1;
+        int z1 = (int)Mth::floor(zCave + rad) - zOffs * 16 + 1;
 
+        if (x0 < 0) x0 = 0;
+        if (x1 > 16) x1 = 16;
         if (y0 < 1) y0 = 1;
         if (y1 > 120) y1 = 120;
+        if (z0 < 0) z0 = 0;
+        if (z1 > 16) z1 = 16;
 
         bool detectedWater = false;
-        for (int xx = x0; !detectedWater && xx <= x1; xx++) {
-            for (int zz = z0; !detectedWater && zz <= z1; zz++) {
+        for (int xx = x0; !detectedWater && xx < x1; xx++) {
+            for (int zz = z0; !detectedWater && zz < z1; zz++) {
                 for (int yy = y1 + 1; !detectedWater && yy >= y0 - 1; yy--) {
+                    int p = (xx * 16 + zz) * 128 + yy;
                     if (yy < 0 || yy >= Level::DEPTH) continue;
-                    int tile = level->getTile(xx, yy, zz);
-                    if (tile == Tile::water->id || tile == Tile::calmWater->id) {
+                    if (blocks[p] == Tile::water->id || blocks[p] == Tile::calmWater->id) {
                         detectedWater = true;
+                    }
+                    if (yy != y0 - 1 && xx != x0 && xx != x1 - 1 && zz != z0 && zz != z1 - 1) {
+                        yy = y0;
                     }
                 }
             }
         }
         if (detectedWater) continue;
 
-        for (int xx = x0; xx <= x1; xx++) {
-            float xd = ((xx + 0.5f) - xCave) / rad;
-            for (int zz = z0; zz <= z1; zz++) {
-                float zd = ((zz + 0.5f) - zCave) / rad;
+        for (int xx = x0; xx < x1; xx++) {
+            float xd = ((xx + xOffs * 16 + 0.5f) - xCave) / rad;
+            for (int zz = z0; zz < z1; zz++) {
+                float zd = ((zz + zOffs * 16 + 0.5f) - zCave) / rad;
+                int p = (xx * 16 + zz) * 128 + y1;
                 bool hasGrass = false;
-                for (int yy = y1; yy >= y0; yy--) {
-                    float yd = (yy + 0.5f - yCave) / yRad;
-                    if (yd > -0.7 && xd * xd + yd * yd + zd * zd < 1) {
-                        int block = level->getTile(xx, yy, zz);
-                        if (block == Tile::grass->id) hasGrass = true;
-                        if (block == Tile::rock->id || block == Tile::dirt->id ||
-                            block == Tile::grass->id) {
-                            if (yy < 10) {
-                                level->setTileNoUpdate(xx, yy, zz, Tile::lava->id);
-                            } else {
-                                level->setTileNoUpdate(xx, yy, zz, 0);
-                                if (hasGrass && level->getTile(xx, yy - 1, zz) == Tile::dirt->id) {
-                                    level->setTileNoUpdate(xx, yy - 1, zz, Tile::grass->id);
+                if (xd * xd + zd * zd < 1.f) {
+                    for (int yy = y1 - 1; yy >= y0; yy--) {
+                        float yd = (yy + 0.5f - yCave) / yRad;
+                        if (yd > -0.7f && xd * xd + yd * yd + zd * zd < 1.f) {
+                            int block = blocks[p];
+                            if (block == Tile::grass->id) hasGrass = true;
+                            if (block == Tile::rock->id || block == Tile::dirt->id || block == Tile::grass->id) {
+                                if (yy < 10) {
+                                    blocks[p] = (unsigned char) Tile::lava->id;
+                                } else {
+                                    blocks[p] = (unsigned char) 0;
+                                    if (hasGrass && blocks[p - 1] == Tile::dirt->id)
+                                        blocks[p - 1] = (unsigned char) Tile::grass->id;
                                 }
                             }
                         }
+                        p--;
                     }
                 }
             }
@@ -135,28 +160,30 @@ void DungeonFeature::addTunnel(Level* level, int xOffs, int zOffs, unsigned char
     }
 }
 
-void DungeonFeature::addFeature(Level* level, int x, int z, int xOffs, int zOffs,
-                                unsigned char* blocks, int /*blocksSize*/) {
+void DungeonFeature::addFeature( Level* level, int x, int z, int xOffs, int zOffs,
+                                 unsigned char* blocks, int /* blocksSize */ )
+{
     int caves = random.nextInt(random.nextInt(random.nextInt(40) + 1) + 1);
     if (random.nextInt(15) != 0) caves = 0;
 
     for (int cave = 0; cave < caves; cave++) {
-        float xCave = x * 16 + random.nextInt(16);
-        float yCave = random.nextInt(random.nextInt(120) + 8);
-        float zCave = z * 16 + random.nextInt(16);
+        float xCave = (float)(x * 16 + random.nextInt(16));
+        float yCave = (float)(random.nextInt(random.nextInt(120) + 8));
+        float zCave = (float)(z * 16 + random.nextInt(16));
 
         int tunnels = 1;
         if (random.nextInt(4) == 0) {
-            addRoom(level, xOffs, zOffs, blocks, xCave, yCave, zCave);
+            addRoom(xOffs, zOffs, blocks, xCave, yCave, zCave);
             tunnels += random.nextInt(4);
         }
 
         for (int i = 0; i < tunnels; i++) {
-            float yRot = random.nextFloat() * Mth::PI * 2;
-            float xRot = ((random.nextFloat() - 0.5f) * 2) / 8;
-            float thickness = random.nextFloat() * 2 + random.nextFloat();
-            addTunnel(level, xOffs, zOffs, blocks, xCave, yCave, zCave,
-                      thickness, yRot, xRot, 0, 0, 1.0);
+            float yRot = random.nextFloat() * Mth::PI * 2.f;
+            float xRot = ((random.nextFloat() - 0.5f) * 2.f) / 8.f;
+            float thickness = random.nextFloat() * 2.f + random.nextFloat();
+
+            addTunnel(xOffs, zOffs, blocks, xCave, yCave, zCave,
+                      thickness, yRot, xRot, 0, 0, 1.0f);
         }
     }
 }
