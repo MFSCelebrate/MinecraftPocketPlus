edf93c7737737b425a704c675170f4a4138b2d20
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-30 20:16:23 +0800

    Update PerlinNoise.h

--- src/world/level/levelgen/synth/PerlinNoise.h
+++ src/world/level/levelgen/synth/PerlinNoise.h
@@ -1,49 +1,42 @@
 #ifndef NET_MINECRAFT_WORLD_LEVEL_LEVELGEN_SYNTH__PerlinNoise_H__
 #define NET_MINECRAFT_WORLD_LEVEL_LEVELGEN_SYNTH__PerlinNoise_H__
 
-//package net.minecraft.world.level.levelgen.synth;
-
 #include "../../../../util/Random.h"
 #include "Synth.h"
 
 class ImprovedNoise;
 
-class PerlinNoise: public Synth
+class PerlinNoise : public Synth
 {
 public:
     PerlinNoise(int levels);
     PerlinNoise(Random* random, int levels);
-	~PerlinNoise();
+    ~PerlinNoise();
 
-    double getValue(double x, double y) const;
+    // 实现 Synth 纯虚函数
+    virtual double getValue(double x, double y) override;
+
+    // 3D 噪声查询（const 版本，供内部使用）
     double getValue(double x, double y, double z) const;
-    double getValue(double x, double y, double z) const;
 
-    // 新增：非 const 版本，满足基类 Synth 纯虚函数
-    inline double getValue(double x, double y) override {
-        return static_cast<const PerlinNoise*>(this)->getValue(x, y);
-    }
-    inline double getValue(double x, double y, double z) {
-        return static_cast<const PerlinNoise*>(this)->getValue(x, y, z);
-    }
-    inline double getValue(double x, double y, double z) {
-        return static_cast<const PerlinNoise*>(this)->getValue(x, y, z);
-	}
-
-	//double[] getRegion(double[] buffer, double x, double y, double z, int xSize, int ySize, int zSize, double xScale, double yScale, double zScale) {
-    double* getRegion(double* buffer, double x, double y, double z, int xSize, int ySize, int zSize, double xScale, double yScale, double zScale);
-    double* getRegion(double* sr, int x, int z, int xSize, int zSize, double xScale, double zScale, double pow);
+    // 批量生成区域（double 缓冲区）
+    double* getRegion(double* buffer, double x, double y, double z,
+                      int xSize, int ySize, int zSize,
+                      double xScale, double yScale, double zScale);
+    double* getRegion(double* sr, int x, int z,
+                      int xSize, int zSize,
+                      double xScale, double zScale, double pow);
 
     int hashCode();
 
 private:
+    void init(int levels);
+
     ImprovedNoise** noiseLevels;
     int levels;
 
     Random _random;
     Random* _rndPtr;
-
-    void init(int levels);
 };
 
-#endif /*NET_MINECRAFT_WORLD_LEVEL_LEVELGEN_SYNTH__PerlinNoise_H__*/
+#endif
