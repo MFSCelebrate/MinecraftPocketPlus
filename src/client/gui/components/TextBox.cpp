c9958e75fc5521a465aaf2a105028df294bf7a76
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-30 18:58:43 +0800

    Update TextBox.cpp

--- src/client/gui/components/TextBox.cpp
+++ src/client/gui/components/TextBox.cpp
@@ -6,18 +6,18 @@
 
 // delegate constructors
 TextBox::TextBox(int id, const std::string& msg)
- : TextBox(id, 0, 0, msg)
+ : TextBox(id, 0, 0, msg), _justTypedDigit(false)
 {
 }
 
 TextBox::TextBox(int id, int x, int y, const std::string& msg)
- : TextBox(id, x, y, 24, Font::DefaultLineHeight + 4, msg)
+ : TextBox(id, x, y, 24, Font::DefaultLineHeight + 4, msg), _justTypedDigit(false)
 {
 }
 
 TextBox::TextBox(int id, int x, int y, int w, int h, const std::string& msg)
  : GuiElement(true, true, x, y, w, h),
-   id(id), hint(msg), focused(false), blink(false), blinkTicks(0)
+   id(id), hint(msg), focused(false), blink(false), blinkTicks(0), _justTypedDigit(false)
 {
 }
 
