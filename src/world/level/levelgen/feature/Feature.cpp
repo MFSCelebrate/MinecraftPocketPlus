b295b29664978ce813744302cad89a0efe65fe28
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-31 12:01:02 +0800

    Update Feature.cpp

--- src/world/level/levelgen/feature/Feature.cpp
+++ src/world/level/levelgen/feature/Feature.cpp
@@ -5,22 +5,17 @@ Feature::Feature( bool doUpdate /*= false*/ )
 {
 }
 
-void Feature::placeBlock( Level* level, int x, int y, int z, int tile )
-{
-	placeBlock(level, x, y, z, tile, 0);
+// ✅ 修改后
+void Feature::placeBlock(Level* level, int64_t x, int y, int64_t z, int tile) {
+    if (doUpdate)
+        level->setTile(x, y, z, tile);
+    else
+        level->setTileNoUpdate(x, y, z, tile);
 }
 
-void Feature::placeBlock( Level* level, int x, int y, int z, int tile, int data )
-{
-	if (doUpdate) {
-		level->setTileAndData(x, y, z, tile, data);
-		/*
-		} else if (level->hasChunkAt(x, y, z) && level->getChunkAt(x, z).seenByPlayer) {
-		if (level->setTileAndDataNoUpdate(x, y, z, tile, data)) {
-		level->sendTileUpdated(x, y, z);
-		}
-		*/
-	} else {
-		level->setTileAndDataNoUpdate(x, y, z, tile, data);
-	}
+void Feature::placeBlock(Level* level, int64_t x, int y, int64_t z, int tile, int data) {
+    if (doUpdate)
+        level->setTileAndData(x, y, z, tile, data);
+    else
+        level->setTileAndDataNoUpdate(x, y, z, tile, data);
 }
