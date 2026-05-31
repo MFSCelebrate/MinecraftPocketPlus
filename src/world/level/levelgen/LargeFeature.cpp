74eb217900cc3991a1b854e540b256a876953d26
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-31 12:52:02 +0800

    Update LargeFeature.cpp

--- src/world/level/levelgen/LargeFeature.cpp
+++ src/world/level/levelgen/LargeFeature.cpp
@@ -2,27 +2,24 @@
 #include <cstdint>
 #include "../Level.h"
 
-LargeFeature::LargeFeature() :	radius(8)
-{
+
+LargeFeature::LargeFeature() : radius(8) {
 }
 
-LargeFeature::~LargeFeature()
-{
+LargeFeature::~LargeFeature() {
 }
 
-void LargeFeature::apply( ChunkSource* chunkSource, Level* level, int xOffs, int zOffs, unsigned char* blocks, int blocksSize )
-{
-	int r = radius;
+void LargeFeature::apply( ChunkSource* chunkSource, Level* level, int64_t xOffs, int64_t zOffs, unsigned char* blocks, int blocksSize ) {
+    int r = radius;
 
-	random.setSeed(level->getSeed());
-	long xScale = random.nextLong() / 2 * 2 + 1;
-	long zScale = random.nextLong() / 2 * 2 + 1;
+    random.setSeed(level->getSeed());
+    long xScale = random.nextLong() / 2 * 2 + 1;
+    long zScale = random.nextLong() / 2 * 2 + 1;
 
-	for (int x = xOffs - r; x <= xOffs + r; x++) {
-		for (int z = zOffs - r; z <= zOffs + r; z++) {
-			random.setSeed((x * xScale + z * zScale) ^ level->getSeed());
-			addFeature(level, x, z, xOffs, zOffs, blocks, blocksSize);
-		}
-	}
+    for (int64_t x = xOffs - r; x <= xOffs + r; x++) {
+        for (int64_t z = zOffs - r; z <= zOffs + r; z++) {
+            random.setSeed((x * xScale + z * zScale) ^ level->getSeed());
+            addFeature(level, x, z, xOffs, zOffs, blocks, blocksSize);
+        }
+    }
 }
-
