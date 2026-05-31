883c2d83caa83549b4507d35fd5c08f89f310efe
MFSCelebrate_ <MFSCelebrate@163.com>
2026-05-31 12:20:32 +0800

    Add files via upload

---
+++ src/world/level/levelgen/CanyonFeature.h
@@ -0,0 +1,17 @@
+#ifndef NET_MINECRAFT_WORLD_LEVEL_LEVELGEN__CanyonFeature_H__
+#define NET_MINECRAFT_WORLD_LEVEL_LEVELGEN__CanyonFeature_H__
+
+#include "LargeFeature.h"
+#include <cstdint>
+
+class CanyonFeature: public LargeFeature {
+public:
+    // 暴露 addFeature，允许外部直接调用
+    virtual void addFeature(Level* level, int x, int z, int xOffs, int zOffs,
+                            unsigned char* blocks, int blocksSize) override;
+protected:
+    void addTunnel(int xOffs, int zOffs, unsigned char* blocks, float xCave, float yCave, float zCave,
+                   float thickness, float yRot, float xRot, int step, int dist, float yScale);
+};
+
+#endif
---
+++ src/world/level/levelgen/LargeFeature.h
@@ -0,0 +1,28 @@
+#ifndef NET_MINECRAFT_WORLD_LEVEL_LEVELGEN__LargeFeature_H__
+#define NET_MINECRAFT_WORLD_LEVEL_LEVELGEN__LargeFeature_H__
+
+//package net.minecraft.world.level.levelgen;
+
+#include "../../../util/Random.h"
+#include <cstdint>
+
+class Random;
+class Level;
+class ChunkSource;
+
+class LargeFeature
+{
+public:
+	LargeFeature();
+	virtual ~LargeFeature();
+
+    virtual void apply(ChunkSource* chunkSource, Level* level, int64_t xOffs, int64_t zOffs, unsigned char* blocks, int blocksSize);
+
+protected:
+    virtual void addFeature(Level* level, int64_t x, int64_t z, int64_t xOffs, int64_t zOffs, unsigned char* blocks, int blocksSize) = 0;
+
+	int radius;
+    Random random;
+};
+
+#endif /*NET_MINECRAFT_WORLD_LEVEL_LEVELGEN__LargeFeature_H__*/
