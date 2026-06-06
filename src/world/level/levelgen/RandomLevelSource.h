2f73b2fc0e27b2bc33eaa2eeb2bae6c784c209b0
MFSCelebrate_ <MFSCelebrate@163.com>
2026-06-06 14:07:11 +0800

    Update RandomLevelSource.h

--- src/world/level/levelgen/RandomLevelSource.h
+++ src/world/level/levelgen/RandomLevelSource.h
@@ -69,7 +69,7 @@ double getWorldScaleZ() const {
 const big_float& getBigWorldScaleX() const { return m_worldScaleX; }
 const big_float& getBigWorldScaleY() const { return m_worldScaleY; }
 const big_float& getBigWorldScaleZ() const { return m_worldScaleZ; }
-const big_float& getBigWorldOffsetX() const { return m_worldOffsetX }
+const big_float& getBigWorldOffsetX() const { return m_worldOffsetX; }
 const big_float& getBigWorldOffsetY() const { return m_worldOffsetY; }
 const big_float& getBigWorldOffsetZ() const { return m_worldOffsetZ; }
 
