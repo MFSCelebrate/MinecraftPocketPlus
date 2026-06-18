1a2ecd9e0745295237fba36ef1f1af8f9c088620
MFSCelebrate_ <MFSCelebrate@163.com>
2026-06-18 21:09:30 +0800

    Update Tile.cpp

--- src/world/level/tile/Tile.cpp
+++ src/world/level/tile/Tile.cpp
@@ -211,8 +211,8 @@ void Tile::initTiles() {
 	emeraldOre  = (new OreTile(56, 16 * 3 + 2))->init()->setDestroyTime(3.0f)->setExplodeable(5)->setSoundType(SOUND_STONE)->setCategory(ItemCategory::Decorations)->setDescriptionId("oreDiamond");
 	emeraldBlock= (new MetalTile(57, 40 - 16))->init()->setDestroyTime(5.0f)->setExplodeable(10)->setSoundType(SOUND_METAL)->setCategory(ItemCategory::Decorations)->setDescriptionId("blockDiamond");
 	workBench   = (new WorkbenchTile(58))->init()->setDestroyTime(2.5f)->setSoundType(SOUND_WOOD)->setCategory(ItemCategory::Structures)->setDescriptionId("workbench");
-	endStone = (new EndStoneTile(121, 12*16+4))  // 白色纹理，临时用
-    ->setDescriptionId("endStone");
+	Tile::endStone = (new EndStoneTile(121, 175))  // terrain.png: x=241, y=161
+    ->setDescriptionId("tile.endStone.name");
 	crops		= (new CropTile(59, 8 + 5 * 16))->init()->setDestroyTime(0.0f)->setSoundType(SOUND_GRASS)->setCategory(ItemCategory::Structures)->setDescriptionId("crops");//->sendTileData();
 	farmland    = (new FarmTile(60))->init()->setDestroyTime(0.6f)->setSoundType(SOUND_GRAVEL)->setCategory(ItemCategory::Structures)->setDescriptionId("farmland");
 	furnace		= (new FurnaceTile(61, false))->init()->setDestroyTime(3.5f)->setSoundType(SOUND_STONE)->setCategory(ItemCategory::Structures)->setDescriptionId("furnace");//.sendTileData();
