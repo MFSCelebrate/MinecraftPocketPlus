8f367ec0b5d276548202f823fc35323a42086af7
MFSCelebrate_ <MFSCelebrate@163.com>
2026-06-19 10:10:53 +0800

    Update TileItem.h

--- src/world/item/TileItem.h
+++ src/world/item/TileItem.h
@@ -1,6 +1,8 @@
 #ifndef NET_MINECRAFT_WORLD_ITEM__TileItem_H__
 #define NET_MINECRAFT_WORLD_ITEM__TileItem_H__
 
+//package net.minecraft.world.item;
+
 #include <string>
 
 #include "Item.h"
@@ -12,73 +14,60 @@
 #include "../../network/RakNetInstance.h"
 #include "../../network/packet/PlaceBlockPacket.h"
 
-class TileItem : public Item
+class TileItem: public Item
 {
-    typedef Item super;
+	typedef Item super;
 
-    int tileId;  // item ID = blockId + 256
-
+	int tileId;
 public:
     TileItem(int id_)
-        : super(id_)
-    {
-        setStackedByData(true);
+	:	super(id_)
+	{
+		setStackedByData(true);
         this->tileId = id_ + 256;
-
-        // 🛡️ id_ 直接就是 blockId，安全索引
-        if (id_ >= 0 && id_ < Tile::NUM_BLOCK_TYPES && Tile::tiles[id_] != nullptr) {
-            this->setIcon(Tile::tiles[id_]->getTexture(2));
-        }
+        this->setIcon(Tile::tiles[id_ + 256]->getTexture(2));
     }
 
     int getTileId() {
         return tileId;
     }
 
-    bool useOn(ItemInstance* instance, Player* player, Level* level,
-               int x, int y, int z, int face,
-               float clickX, float clickY, float clickZ)
-    {
-        if (level->adventureSettings.immutableWorld) {
-            const Tile* tile = Tile::tiles[tileId - 256];
-            if (tileId != ((Tile*)Tile::leaves)->id
-                && tile->material != Material::plant) {
-                return false;
-            }
-        }
+    bool useOn(ItemInstance* instance, Player* player, Level* level, int x, int y, int z, int face, float clickX, float clickY, float clickZ) {
+		if (level->adventureSettings.immutableWorld) {
+			const Tile* tile = Tile::tiles[tileId];
+			if (tileId != ((Tile*)Tile::leaves)->id
+				&& tile->material != Material::plant) {
+					return false;
+			}
+		}
 
-        if (level->getTile(x, y, z) == Tile::topSnow->id) {
+		if (level->getTile(x, y, z) == Tile::topSnow->id) {
             face = 0;
         } else {
-            switch (face) {
-                case Facing::DOWN : y--; break;
+			switch (face) {
+				case Facing::DOWN : y--; break;
 				case Facing::UP   : y++; break;
 				case Facing::NORTH: z--; break;
 				case Facing::SOUTH: z++; break;
 				case Facing::WEST : x--; break;
 				case Facing::EAST : x++; break;
-            }
+			}
         }
 
         if (instance->count == 0) return false;
 
         if (level->mayPlace(tileId, x, y, z, false, face)) {
-
-            Tile* tile = Tile::tiles[tileId - 256];
-
-            int data = tile->getPlacedOnFaceDataValue(level, x, y, z, face,
-                          clickX, clickY, clickZ,
-                          getLevelDataForAuxValue(instance->getAuxValue()));
-
+            Tile* tile = Tile::tiles[tileId];
+			int data = tile->getPlacedOnFaceDataValue(level, x, y, z, face, clickX, clickY, clickZ, getLevelDataForAuxValue(instance->getAuxValue()));
             if (level->setTileAndData(x, y, z, tileId, data)) {
+                Tile::tiles[tileId]->setPlacedBy(level, x, y, z, player);
+                level->playSound(x + 0.5f, y + 0.5f, z + 0.5f, tile->soundType->getStepSound(), (tile->soundType->getVolume() + 1) / 2, tile->soundType->getPitch() * 0.8f);
 
-                Tile::tiles[tileId - 256]->setPlacedBy(level, x, y, z, player);
-
-                level->playSound(x + 0.5f, y + 0.5f, z + 0.5f,
-                    tile->soundType->getStepSound(),
-                    (tile->soundType->getVolume() + 1) / 2,
-                    tile->soundType->getPitch() * 0.8f);
-
+/*
+				PlaceBlockPacket packet(player->entityId, x, y, z, face, tileId, instance->getAuxValue());
+				//LOGI("Place block at @ %d, %d, %d\n", x, y, z);
+				level->raknetInstance->send(packet);
+*/
                 instance->count--;
             }
             return true;
@@ -87,12 +76,12 @@ public:
     }
 
     std::string getDescriptionId(const ItemInstance* instance) const {
-        return Tile::tiles[tileId - 256]->getDescriptionId();
+        return Tile::tiles[tileId]->getDescriptionId();
     }
 
     std::string getDescriptionId() const {
-        return Tile::tiles[tileId - 256]->getDescriptionId();
+        return Tile::tiles[tileId]->getDescriptionId();
     }
 };
 
-#endif
+#endif /*NET_MINECRAFT_WORLD_ITEM__TileItem_H__*/
