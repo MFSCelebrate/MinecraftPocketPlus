561fb063de2e73bf211ddbd9f2c5272ae24b094d
MFSCelebrate_ <MFSCelebrate@163.com>
2026-06-20 18:30:25 +0800

    Update FoliageColor.h

--- src/world/level/FoliageColor.h
+++ src/world/level/FoliageColor.h
@@ -4,6 +4,9 @@
 class FoliageColor
 {
 public:
+    static int getEndGrassColor()    { return 0x8EB971; }
+    static int getEndFoliageColor()  { return 0x71A74D; }
+    static int getEndDeadColor()     { return 0xA17448; }
     // Beta 1.7.3 草木色调
     static int getEvergreenColor()  { return 0x6A9C4A; }  // 原 0x619961
     static int getBirchColor()      { return 0x9CCE6C; }  // 原 0x80a755
