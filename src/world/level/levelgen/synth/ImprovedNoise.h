4e5fe4d3ab280721fcf552c154e78e3e283b91c8
MFSCelebrate_ <MFSCelebrate@163.com>
2026-06-22 10:50:52 +0800

    Update ImprovedNoise.h

--- src/world/level/levelgen/synth/ImprovedNoise.h
+++ src/world/level/levelgen/synth/ImprovedNoise.h
@@ -1,40 +1,44 @@
 #ifndef NET_MINECRAFT_WORLD_LEVEL_LEVELGEN_SYNTH__ImprovedNoise_H__
 #define NET_MINECRAFT_WORLD_LEVEL_LEVELGEN_SYNTH__ImprovedNoise_H__
 
-//package net.minecraft.world.level.levelgen.synth;
-
 #include "Synth.h"
-class Random;
+#include "../../../../util/Random.h"
 
-class ImprovedNoise: public Synth
-{
+class ImprovedNoise : public Synth {
 public:
     ImprovedNoise();
-
     ImprovedNoise(Random* random);
-	
-	void init(Random* random);
 
-    float noise(float _x, float _y, float _z) const;
+    void init(Random* random);
 
-    const float lerp(float t, float a, float b) const;
-
-    const float grad2(int hash, float x, float z) const;
-    const float grad(int hash, float x, float y, float z) const;
-    float getValue(float x, float y) override;          // 满足 Synth 纯虚函数
+    // ---- 原版 float 接口（保留） ----
+    float noise(float x, float y, float z) const;
+    float getValue(float x, float y) override;
     float getValue(float x, float y) const;
     float getValue(float x, float y, float z) const;
-    float getValue(double x, double y, double z) const;
-    float getValue(double x, double y) const;          // 2D double 版本
 
-    // 修改第 46 行附近
-void add(float *buffer, double _x, double _y, double _z, int xSize, int ySize, int zSize, float xs, float ys, float zs, float pow);
+    // ---- 新增 double 接口 ----
+    double getValue(double x, double y, double z) const;
+    double getValue(double x, double y) const;
+
+    // ---- 核心数学函数（重载 double 版本） ----
+    double lerp(double t, double a, double b) const;
+    double grad2(int hash, double x, double z) const;
+    double grad(int hash, double x, double y, double z) const;
+
+    // ---- 保留原 float 版本（供原版调用） ----
+    float lerp(float t, float a, float b) const;
+    float grad2(int hash, float x, float z) const;
+    float grad(int hash, float x, float y, float z) const;
+
+    void add(float* buffer, double _x, double _y, double _z,
+             int xSize, int ySize, int zSize,
+             float xs, float ys, float zs, float pow);
 
     int hashCode();
 
-	float scale;
-	float xo, yo, zo;
+    float xo, yo, zo;
     int p[512];
 };
 
-#endif /*NET_MINECRAFT_WORLD_LEVEL_LEVELGEN_SYNTH__ImprovedNoise_H__*/
+#endif
