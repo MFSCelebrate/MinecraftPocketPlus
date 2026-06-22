360ec57d563f159f8beb0e2f84bf938b72eb5b78
MFSCelebrate_ <MFSCelebrate@163.com>
2026-06-22 12:45:03 +0800

    Update RandomLevelSource.cpp

--- src/world/level/levelgen/RandomLevelSource.cpp
+++ src/world/level/levelgen/RandomLevelSource.cpp
@@ -194,11 +194,16 @@ void RandomLevelSource::buildSurfaces(double xOffs, double zOffs, unsigned char*
         double coordZ = (zOffs / 4.0 + z) * sz;
         
         // RandomLevelSource.cpp 第 220 行附近
-sandBuffer[x + z * 16] = perlinNoise2.getValueDouble(coordX, 0.0, coordZ);
-gravelBuffer[x + z * 16] = perlinNoise2.getValueDouble(coordX, 109.01340, coordZ);
-depthBuffer[x + z * 16] = perlinNoise3.getValueDouble(coordX * 2.0, 0.0, coordZ * 2.0);
+for (int x = 0; x < 16; x++) {
+    for (int z = 0; z < 16; z++) {
+        double coordX = (xOffs / 4.0 + x) * sx;
+        double coordZ = (zOffs / 4.0 + z) * sz;
+        
+        sandBuffer[x + z * 16]   = perlinNoise2.getValue(coordX, 0.0, coordZ);
+        gravelBuffer[x + z * 16] = perlinNoise2.getValue(coordX, 109.01340, coordZ);
+        depthBuffer[x + z * 16]  = perlinNoise3.getValue(coordX * 2.0, 0.0, coordZ * 2.0);
     }
-	}
+}
     for (int x = 0; x < 16; x++) {
         for (int z = 0; z < 16; z++) {
             double temp = 1.0;
